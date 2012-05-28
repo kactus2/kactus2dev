e512eb7e468a61f0b6af0c1f4ca8f200cefafb85
Antti Kamppi <antti.kamppi@tut.fi>
2012-05-28 13:53:34 +0000

    [PERFECTIVE] Added a NameGroupEditor to edit directly the NameGroup-struct
    git-svn-id: svn://svn.code.sf.net/p/kactus2/code/trunk@297 f0aa6db5-7f46-43cc-a8a5-a31efdaafa67

---
+++ common/widgets/nameGroupEditor/namegroupeditor.cpp
@@ -0,0 +1,84 @@
+/* 
+ *  	Created on: 28.5.2012
+ *      Author: Antti Kamppi
+ * 		filename: namegroupeditor.cpp
+ *		Project: Kactus 2
+ */
+
+#include "namegroupeditor.h"
+#include <common/validators/nameValidator/namevalidator.h>
+
+#include <QLabel>
+#include <QGridLayout>
+
+NameGroupEditor::NameGroupEditor( General::NameGroup& nameGroup,
+								 QWidget *parent,
+								 const QString& title /*= QString("Names")*/ ):
+QGroupBox(title, parent),
+nameGroup_(nameGroup),
+nameEdit_(this),
+displayNameEdit_(this),
+descriptionEdit_(this) {
+
+	// the layout manager for this widget
+	QGridLayout* layout = new QGridLayout(this);
+
+	QLabel* nameLabel = new QLabel(tr("Name:"), this);
+	layout->addWidget(nameLabel, 0, 0, 1, 1);
+
+	nameEdit_.setToolTip(tr("Set the name for the element"));
+	nameEdit_.setProperty("mandatoryField", true);
+
+	NameValidator* validator = new NameValidator(&nameEdit_);
+	nameEdit_.setValidator(validator);
+	layout->addWidget(&nameEdit_, 0, 1, 1, 1);
+
+	QLabel* displayLabel = new QLabel(tr("Display Name:"), this);
+	layout->addWidget(displayLabel, 1, 0, 1, 1);
+
+	displayNameEdit_.setToolTip(tr("Set the display name for the element"));
+	layout->addWidget(&displayNameEdit_, 1, 1, 1, 1);
+
+	QLabel* descriptionLabel = new QLabel(tr("Description:"), this);
+	layout->addWidget(descriptionLabel, 2, 0, 1, 1);
+
+	descriptionEdit_.setToolTip(tr("Set the description for the element"));
+	layout->addWidget(&descriptionEdit_, 3, 0, 1, 2);
+
+	connect(&nameEdit_, SIGNAL(textEdited(const QString&)),
+		this, SIGNAL(onNameChanged(const QString&)), Qt::UniqueConnection);
+	connect(&displayNameEdit_, SIGNAL(textEdited(const QString&)),
+		this, SIGNAL(onDisplayNameChanged(const QString&)), Qt::UniqueConnection);
+	connect(&descriptionEdit_, SIGNAL(textChanged()),
+		this, SLOT(onDescriptionChanged()), Qt::UniqueConnection);
+}
+
+NameGroupEditor::~NameGroupEditor() {
+}
+
+QString NameGroupEditor::getName() const {
+	return nameGroup_.name_;
+}
+
+QString NameGroupEditor::getDisplayName() const {
+	return nameGroup_.displayName_;
+}
+
+QString NameGroupEditor::getDescription() const {
+	return nameGroup_.description_;
+}
+
+void NameGroupEditor::onNameChanged( const QString& newName ) {
+	nameGroup_.name_ = newName;
+	emit contentChanged();
+}
+
+void NameGroupEditor::onDisplayNameChanged( const QString& newName ) {
+	nameGroup_.displayName_ = newName;
+	emit contentChanged();
+}
+
+void NameGroupEditor::onDescriptionChanged() {
+	nameGroup_.description_ = descriptionEdit_.toPlainText();
+	emit contentChanged();
+}
---
+++ common/widgets/nameGroupEditor/namegroupeditor.h
@@ -0,0 +1,104 @@
+/* 
+ *  	Created on: 28.5.2012
+ *      Author: Antti Kamppi
+ * 		filename: namegroupeditor.h
+ *		Project: Kactus 2
+ */
+
+#ifndef NAMEGROUPEDITOR_H
+#define NAMEGROUPEDITOR_H
+
+#include <models/generaldeclarations.h>
+
+#include <QGroupBox>
+#include <QLineEdit>
+#include <QPlainTextEdit>
+#include <QString>
+
+/*! \brief Editor to edit the details of a NameGroup-struct.
+ *
+ */
+class NameGroupEditor : public QGroupBox {
+	Q_OBJECT
+
+public:
+
+	/*! \brief The constructor
+	 *
+	 * \param nameGroup Reference to the name group to edit.
+	 * \param parent Pointer to the owner of this widget.
+	 * \param title The title for the group box.
+	 *
+	*/
+	NameGroupEditor(General::NameGroup& nameGroup,
+		QWidget *parent,
+		const QString& title = QString("Names"));
+
+	//! \brief The destructor
+	~NameGroupEditor();
+
+	/*! \brief Get the name of the name group.
+	 *
+	 * \return The contents of the name field.
+	*/
+	QString getName() const;
+
+	/*! \brief Get the display name of the name group.
+	 *
+	 * \return The contents of the display name field.
+	*/
+	QString getDisplayName() const;
+
+	/*! \brief Get the description of the name group.
+	 *
+	 * \return The contents of the description field.
+	*/
+	QString getDescription() const;
+
+signals:
+
+	//! \brief Emitted when user edits one of the fields.
+	void contentChanged();
+
+private slots:
+
+	/*! \brief Handler for editing of the name field.
+	 *
+	 * \param newName The new name for the name group.
+	 *
+	*/
+	void onNameChanged(const QString& newName);
+
+	/*! \brief Handler for editing of the display name field.
+	 *
+	 * \param newName The new display name for the name group.
+	 *
+	*/
+	void onDisplayNameChanged(const QString& newName);
+
+	/*! \brief Handler for editing of the description field.
+	 *
+	*/
+	void onDescriptionChanged();
+
+private:
+	//! \brief No copying
+	NameGroupEditor(const NameGroupEditor& other);
+
+	//! \brief No assignment
+	NameGroupEditor& operator=(const NameGroupEditor& other);
+
+	//! \brief The name group to edit.
+	General::NameGroup& nameGroup_;
+
+	//! \brief Line edit to set the name.
+	QLineEdit nameEdit_;
+
+	//! \brief Line edit to set the displayName.
+	QLineEdit displayNameEdit_;
+
+	//! \brief Editor to write the description.
+	QPlainTextEdit descriptionEdit_;
+};
+
+#endif // NAMEGROUPEDITOR_H
