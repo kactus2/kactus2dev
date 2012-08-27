1724b12a0032f55aa2a53253646a2930d199757d
Joni-Matti Määttä <joni-matti.maatta@tut.fi>
2012-08-27 09:53:40 +0000

    [CORRECTIVE] Added missing icon files.
    git-svn-id: svn://svn.code.sf.net/p/kactus2/code/trunk@498 f0aa6db5-7f46-43cc-a8a5-a31efdaafa67

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           2.0.752.0
+#define VERSION_FULL           2.0.754.0
 
 #define VERSION_BASEYEAR       0
 #define VERSION_DATE           "2012-08-27"
-#define VERSION_TIME           "09:22:24"
+#define VERSION_TIME           "12:34:18"
 
 #define VERSION_MAJOR          2
 #define VERSION_MINOR          0
-#define VERSION_BUILDNO        752
+#define VERSION_BUILDNO        754
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           2,0,752,0
-#define VERSION_PRODUCT        2,0,752,0
-#define VERSION_FILESTR        "2,0,752,0"
-#define VERSION_PRODUCTSTR     "2,0,752,0"
+#define VERSION_FILE           2,0,754,0
+#define VERSION_PRODUCT        2,0,754,0
+#define VERSION_FILESTR        "2,0,754,0"
+#define VERSION_PRODUCTSTR     "2,0,754,0"
 
 #endif
