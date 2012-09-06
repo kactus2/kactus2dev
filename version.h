ed25e81242d447fb5ce03a545b64e66166e0f16c
Antti Kamppi <antti.kamppi@tut.fi>
2012-09-06 13:46:46 +0000

    git-svn-id: svn://svn.code.sf.net/p/kactus2/code/trunk@528 f0aa6db5-7f46-43cc-a8a5-a31efdaafa67

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           2.0.969.0
+#define VERSION_FULL           2.0.972.0
 
 #define VERSION_BASEYEAR       0
-#define VERSION_DATE           "2012-09-05"
-#define VERSION_TIME           "15:23:18"
+#define VERSION_DATE           "2012-09-06"
+#define VERSION_TIME           "16:09:18"
 
 #define VERSION_MAJOR          2
 #define VERSION_MINOR          0
-#define VERSION_BUILDNO        969
+#define VERSION_BUILDNO        972
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           2,0,969,0
-#define VERSION_PRODUCT        2,0,969,0
-#define VERSION_FILESTR        "2,0,969,0"
-#define VERSION_PRODUCTSTR     "2,0,969,0"
+#define VERSION_FILE           2,0,972,0
+#define VERSION_PRODUCT        2,0,972,0
+#define VERSION_FILESTR        "2,0,972,0"
+#define VERSION_PRODUCTSTR     "2,0,972,0"
 
 #endif
