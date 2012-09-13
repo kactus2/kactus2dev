727665aefdd6d87e6d43fbb0a37120c943a765d2
Antti Kamppi <antti.kamppi@tut.fi>
2012-09-13 14:26:50 +0000

    [DOCUMENTATION] Updated UML diagrams
    git-svn-id: svn://svn.code.sf.net/p/kactus2/code/trunk@547 f0aa6db5-7f46-43cc-a8a5-a31efdaafa67

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           2.0.7.0
+#define VERSION_FULL           2.0.8.0
 
 #define VERSION_BASEYEAR       0
 #define VERSION_DATE           "2012-09-13"
-#define VERSION_TIME           "14:58:40"
+#define VERSION_TIME           "17:13:01"
 
 #define VERSION_MAJOR          2
 #define VERSION_MINOR          0
-#define VERSION_BUILDNO        7
+#define VERSION_BUILDNO        8
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           2,0,7,0
-#define VERSION_PRODUCT        2,0,7,0
-#define VERSION_FILESTR        "2,0,7,0"
-#define VERSION_PRODUCTSTR     "2,0,7,0"
+#define VERSION_FILE           2,0,8,0
+#define VERSION_PRODUCT        2,0,8,0
+#define VERSION_FILESTR        "2,0,8,0"
+#define VERSION_PRODUCTSTR     "2,0,8,0"
 
 #endif
