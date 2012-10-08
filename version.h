bf03fef59386f3256f9a5810878d3609eaa1c0cc
Joni-Matti Määttä <joni-matti.maatta@tut.fi>
2012-10-08 16:18:04 +0000

    [ADAPTIVE] Added VS2010 project files (not perfect yet).
    git-svn-id: svn://svn.code.sf.net/p/kactus2/code/trunk@567 f0aa6db5-7f46-43cc-a8a5-a31efdaafa67

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           2.0.243.0
+#define VERSION_FULL           2.0.245.0
 
 #define VERSION_BASEYEAR       0
 #define VERSION_DATE           "2012-10-08"
-#define VERSION_TIME           "18:28:51"
+#define VERSION_TIME           "19:12:00"
 
 #define VERSION_MAJOR          2
 #define VERSION_MINOR          0
-#define VERSION_BUILDNO        243
+#define VERSION_BUILDNO        245
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           2,0,243,0
-#define VERSION_PRODUCT        2,0,243,0
-#define VERSION_FILESTR        "2,0,243,0"
-#define VERSION_PRODUCTSTR     "2,0,243,0"
+#define VERSION_FILE           2,0,245,0
+#define VERSION_PRODUCT        2,0,245,0
+#define VERSION_FILESTR        "2,0,245,0"
+#define VERSION_PRODUCTSTR     "2,0,245,0"
 
 #endif
