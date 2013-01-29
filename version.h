7396bb3d9d4dd6a3c34b99811b71c1fa5894b00e
Joni-Matti Määttä <joni-matti.maatta@tut.fi>
2013-01-29 16:31:03 +0000

    [CORRECTIVE] Fixed x64 compilation problems in the new VS2010 project file.
    git-svn-id: svn://svn.code.sf.net/p/kactus2/code/trunk@745 f0aa6db5-7f46-43cc-a8a5-a31efdaafa67

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           2.0.1273.0
+#define VERSION_FULL           2.0.1278.0
 
 #define VERSION_BASEYEAR       0
 #define VERSION_DATE           "2013-01-29"
-#define VERSION_TIME           "10:57:13"
+#define VERSION_TIME           "18:27:48"
 
 #define VERSION_MAJOR          2
 #define VERSION_MINOR          0
-#define VERSION_BUILDNO        1273
+#define VERSION_BUILDNO        1278
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           2,0,1273,0
-#define VERSION_PRODUCT        2,0,1273,0
-#define VERSION_FILESTR        "2,0,1273,0"
-#define VERSION_PRODUCTSTR     "2,0,1273,0"
+#define VERSION_FILE           2,0,1278,0
+#define VERSION_PRODUCT        2,0,1278,0
+#define VERSION_FILESTR        "2,0,1278,0"
+#define VERSION_PRODUCTSTR     "2,0,1278,0"
 
 #endif
