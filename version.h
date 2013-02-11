79abc029855f213115c6ec7b2b9e9f78f8b84ba2
Antti Kamppi <antti.kamppi@tut.fi>
2013-02-11 16:15:28 +0000

    [ADAPTIVE] Changed whole solution to use qt 5.0.1
    git-svn-id: svn://svn.code.sf.net/p/kactus2/code/trunk@806 f0aa6db5-7f46-43cc-a8a5-a31efdaafa67

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           2.1.182.0
+#define VERSION_FULL           2.1.191.0
 
 #define VERSION_BASEYEAR       0
 #define VERSION_DATE           "2013-02-11"
-#define VERSION_TIME           "14:20:21"
+#define VERSION_TIME           "18:13:39"
 
 #define VERSION_MAJOR          2
 #define VERSION_MINOR          1
-#define VERSION_BUILDNO        182
+#define VERSION_BUILDNO        191
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           2,1,182,0
-#define VERSION_PRODUCT        2,1,182,0
-#define VERSION_FILESTR        "2,1,182,0"
-#define VERSION_PRODUCTSTR     "2,1,182,0"
+#define VERSION_FILE           2,1,191,0
+#define VERSION_PRODUCT        2,1,191,0
+#define VERSION_FILESTR        "2,1,191,0"
+#define VERSION_PRODUCTSTR     "2,1,191,0"
 
 #endif
