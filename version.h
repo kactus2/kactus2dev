c094582a3e4130378763abfdb5a0271311de0803
Joni-Matti Määttä <joni-matti.maatta@tut.fi>
2013-02-18 08:14:13 +0000

    [CORRECTIVE] Added correct project dependencies to the new memory map generator project to ensure successful compilations.
    git-svn-id: svn://svn.code.sf.net/p/kactus2/code/trunk@823 f0aa6db5-7f46-43cc-a8a5-a31efdaafa67

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           2.1.217.0
+#define VERSION_FULL           2.1.221.0
 
 #define VERSION_BASEYEAR       0
-#define VERSION_DATE           "2013-02-15"
-#define VERSION_TIME           "14:43:40"
+#define VERSION_DATE           "2013-02-18"
+#define VERSION_TIME           "10:13:13"
 
 #define VERSION_MAJOR          2
 #define VERSION_MINOR          1
-#define VERSION_BUILDNO        217
+#define VERSION_BUILDNO        221
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           2,1,217,0
-#define VERSION_PRODUCT        2,1,217,0
-#define VERSION_FILESTR        "2,1,217,0"
-#define VERSION_PRODUCTSTR     "2,1,217,0"
+#define VERSION_FILE           2,1,221,0
+#define VERSION_PRODUCT        2,1,221,0
+#define VERSION_FILESTR        "2,1,221,0"
+#define VERSION_PRODUCTSTR     "2,1,221,0"
 
 #endif
