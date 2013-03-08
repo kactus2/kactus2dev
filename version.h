538c9e212f29913caca2ea08d9fc3d143aa51117
Antti Kamppi <antti.kamppi@tut.fi>
2013-03-08 13:15:28 +0000

    [PERFECTIVE] Added some sections to the Kactus2 help
    git-svn-id: svn://svn.code.sf.net/p/kactus2/code/trunk@881 f0aa6db5-7f46-43cc-a8a5-a31efdaafa67

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           2.1.536.0
+#define VERSION_FULL           2.1.546.0
 
 #define VERSION_BASEYEAR       0
 #define VERSION_DATE           "2013-03-08"
-#define VERSION_TIME           "14:49:40"
+#define VERSION_TIME           "15:14:26"
 
 #define VERSION_MAJOR          2
 #define VERSION_MINOR          1
-#define VERSION_BUILDNO        536
+#define VERSION_BUILDNO        546
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           2,1,536,0
-#define VERSION_PRODUCT        2,1,536,0
-#define VERSION_FILESTR        "2,1,536,0"
-#define VERSION_PRODUCTSTR     "2,1,536,0"
+#define VERSION_FILE           2,1,546,0
+#define VERSION_PRODUCT        2,1,546,0
+#define VERSION_FILESTR        "2,1,546,0"
+#define VERSION_PRODUCTSTR     "2,1,546,0"
 
 #endif
