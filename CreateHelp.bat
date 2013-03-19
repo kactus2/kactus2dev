7e3036c113eec612a14a753e07e83ac94d02977f
Erno Salminen <erno.salminen@tut.fi>
2013-03-19 11:34:10 +0000

    [PERFECTIVE] Added a short header to the script
    git-svn-id: svn://svn.code.sf.net/p/kactus2/code/trunk@910 f0aa6db5-7f46-43cc-a8a5-a31efdaafa67

--- CreateHelp.bat
+++ CreateHelp.bat
@@ -1,3 +1,8 @@
+#
+# Bind HTML pages to context helps. If you modify help texts, you
+# don't need recompilation, if you run this script.
+# Joni-Matti M‰‰tt‰, Aug 2012
+#
 @echo off
 qhelpgenerator Help/kactus2help.qhp -o Help/Kactus2Help.qch
 qcollectiongenerator Help/kactus2help.qhcp -o Help/Kactus2Help.qhc
