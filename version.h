7b49f5f69b9ec0210676f18f2d08d5f73671d943
Antti Kamppi <antti.kamppi@tut.fi>
2013-04-04 12:11:03 +0000

    [PERFECTIVE] Upgraded the version configuration file for Kactus2 settings
    git-svn-id: svn://svn.code.sf.net/p/kactus2/code/trunk@947 f0aa6db5-7f46-43cc-a8a5-a31efdaafa67

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           2.1.722.0
+#define VERSION_FULL           2.1.723.0
 
 #define VERSION_BASEYEAR       0
 #define VERSION_DATE           "2013-04-04"
-#define VERSION_TIME           "14:30:39"
+#define VERSION_TIME           "14:32:30"
 
 #define VERSION_MAJOR          2
 #define VERSION_MINOR          1
-#define VERSION_BUILDNO        722
+#define VERSION_BUILDNO        723
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           2,1,722,0
-#define VERSION_PRODUCT        2,1,722,0
-#define VERSION_FILESTR        "2,1,722,0"
-#define VERSION_PRODUCTSTR     "2,1,722,0"
+#define VERSION_FILE           2,1,723,0
+#define VERSION_PRODUCT        2,1,723,0
+#define VERSION_FILESTR        "2,1,723,0"
+#define VERSION_PRODUCTSTR     "2,1,723,0"
 
 #endif
