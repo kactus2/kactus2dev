03b7ec019a43ad555d371a12b24bc858e2bdae88
Joni-Matti Määttä <joni-matti.maatta@tut.fi>
2013-04-15 10:01:42 +0000

    [ADAPTIVE] Migrated 64-bit version to Qt 5.0.2 x64.
    git-svn-id: svn://svn.code.sf.net/p/kactus2/code/trunk@977 f0aa6db5-7f46-43cc-a8a5-a31efdaafa67

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           2.1.851.0
+#define VERSION_FULL           2.1.836.0
 
 #define VERSION_BASEYEAR       0
-#define VERSION_DATE           "2013-04-12"
-#define VERSION_TIME           "17:07:46"
+#define VERSION_DATE           "2013-04-15"
+#define VERSION_TIME           "12:46:17"
 
 #define VERSION_MAJOR          2
 #define VERSION_MINOR          1
-#define VERSION_BUILDNO        851
+#define VERSION_BUILDNO        836
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           2,1,851,0
-#define VERSION_PRODUCT        2,1,851,0
-#define VERSION_FILESTR        "2,1,851,0"
-#define VERSION_PRODUCTSTR     "2,1,851,0"
+#define VERSION_FILE           2,1,836,0
+#define VERSION_PRODUCT        2,1,836,0
+#define VERSION_FILESTR        "2,1,836,0"
+#define VERSION_PRODUCTSTR     "2,1,836,0"
 
 #endif
