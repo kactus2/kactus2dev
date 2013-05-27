53cec35296a598246c6735076a08a01fa6032877
Timo Korpela <timo.korpela@outdated.tut.fi>
2013-05-27 22:00:37 +0000

    [CORRECTIVE] Fixed comments to right style
    git-svn-id: svn://svn.code.sf.net/p/kactus2/code/trunk@1061 f0aa6db5-7f46-43cc-a8a5-a31efdaafa67

--- Vhdl2IPXact/textedit.cpp
+++ Vhdl2IPXact/textedit.cpp
@@ -1,17 +1,36 @@
+//-----------------------------------------------------------------------------
+// File: textedit.cpp
+//-----------------------------------------------------------------------------
+// Project: Kactus 2
+// Author: Timo Korpela
+// Date: 28.05.2013
+//
+// Description:
+// QTextEdit with custom functionality
+//-----------------------------------------------------------------------------
 #include <QTextCursor>
 #include "textedit.h"
 
+
+//-----------------------------------------------------------------------------
+// Function: textEdit::textEdit()
+//-----------------------------------------------------------------------------
 textEdit::textEdit( QWidget *parent ) : QTextEdit( parent )
 {
     setReadOnly( true );
     setCursorWidth(0);
-
 }
 
+//-----------------------------------------------------------------------------
+// Function: textEdit::~textEdit()
+//-----------------------------------------------------------------------------
 textEdit::~textEdit()
 {
 }
 
+//-----------------------------------------------------------------------------
+// Function: textEdit::mouseDoubleClickEvent()
+//-----------------------------------------------------------------------------
 void textEdit::mouseDoubleClickEvent(QMouseEvent *e) {
 
     QTextCursor textc = cursorForPosition( e->pos() );
@@ -19,5 +38,5 @@ void textEdit::mouseDoubleClickEvent(QMouseEvent *e) {
     QString worduc = textc.selectedText();
     if ( !worduc.isEmpty() ) {
          emit selectedWord( worduc );
-}
+    }
 }
--- Vhdl2IPXact/textedit.h
+++ Vhdl2IPXact/textedit.h
@@ -1,28 +1,48 @@
+//-----------------------------------------------------------------------------
+// File: textedit.h
+//-----------------------------------------------------------------------------
+// Project: Kactus 2
+// Author: Timo Korpela
+// Date: 24.05.2013
+//
+// Description:
+// header file introducing textedit class
+//-----------------------------------------------------------------------------
 #ifndef TEXTEDIT_H
 #define TEXTEDIT_H
 
 #include <QTextEdit>
 #include <QMouseEvent>
 
+
+//-----------------------------------------------------------------------------
+//! Class for text display. based to QTextEdit
+//-----------------------------------------------------------------------------
 class textEdit : public QTextEdit
 {
     Q_OBJECT
 public:
 
-    //standard constructor and deconstructor
-    //only addition: make text read only
+    /*!
+    *   standard constructor and deconstructor
+    *   only addition: make text read only
+    */
     textEdit( QWidget *parent = 0);
     ~textEdit();
 signals:
 
-    //signal to get function call in main class
+    /*!
+    *   signal to get function call in main class
+    */
     void selectedWord (const QString &);
+
 protected:
 
-    //implemention of mouse event for textEdit
-    //
+    /*!
+     * Custom mouseDoubleClickEvent handler
+     * \param e QMouseEventfrom position of doubleclick
+     */
     virtual void mouseDoubleClickEvent(QMouseEvent *e);
-    //virtual void mousePressEvent(QMouseEvent *e);
 
 };
 
