f225a097822e24d8e184332e3aad924523ff7ea6
Joni-Matti Määttä <joni-matti.maatta@tut.fi>
2013-07-05 06:35:22 +0000

    [ADAPTIVE] Migrated x64 targets to Qt5.1.0x64.
    git-svn-id: svn://svn.code.sf.net/p/kactus2/code/trunk@1166 f0aa6db5-7f46-43cc-a8a5-a31efdaafa67

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           2.1.1830.0
+#define VERSION_FULL           2.1.1832.0
 
 #define VERSION_BASEYEAR       0
-#define VERSION_DATE           "2013-07-04"
-#define VERSION_TIME           "13:55:09"
+#define VERSION_DATE           "2013-07-05"
+#define VERSION_TIME           "09:32:46"
 
 #define VERSION_MAJOR          2
 #define VERSION_MINOR          1
-#define VERSION_BUILDNO        1830
+#define VERSION_BUILDNO        1832
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           2,1,1830,0
-#define VERSION_PRODUCT        2,1,1830,0
-#define VERSION_FILESTR        "2,1,1830,0"
-#define VERSION_PRODUCTSTR     "2,1,1830,0"
+#define VERSION_FILE           2,1,1832,0
+#define VERSION_PRODUCT        2,1,1832,0
+#define VERSION_FILESTR        "2,1,1832,0"
+#define VERSION_PRODUCTSTR     "2,1,1832,0"
 
 #endif
