20973e7b064a14528f5025284ddf85e580a371dc
Esko Pekkarinen <esko.pekkarinen@tut.fi>
2013-09-20 06:46:09 +0000

    [PERFECTIVE] Updated upgrade files and version for release.
    git-svn-id: svn://svn.code.sf.net/p/kactus2/code/trunk@1290 f0aa6db5-7f46-43cc-a8a5-a31efdaafa67

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           2.2.1149.0
+#define VERSION_FULL           2.3.0.0
 
 #define VERSION_BASEYEAR       0
 #define VERSION_DATE           "2013-09-19"
 #define VERSION_TIME           "12:07:27"
 
 #define VERSION_MAJOR          2
-#define VERSION_MINOR          2
-#define VERSION_BUILDNO        1149
+#define VERSION_MINOR          3
+#define VERSION_BUILDNO        0
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           2,2,1149,0
-#define VERSION_PRODUCT        2,2,1149,0
-#define VERSION_FILESTR        "2,2,1149,0"
-#define VERSION_PRODUCTSTR     "2,2,1149,0"
+#define VERSION_FILE           2,3,0,0
+#define VERSION_PRODUCT        2,3,0,0
+#define VERSION_FILESTR        "2,3,0,0"
+#define VERSION_PRODUCTSTR     "2,3,0,0"
 
 #endif
