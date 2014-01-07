59b5fe82572614f886ca21ced947aa615de83d77
Esko Pekkarinen <esko.pekkarinen@tut.fi>
2014-01-07 10:09:27 +0000

    [ADDITIVE] Added view for ports editor to fix compilation error.
    git-svn-id: svn://svn.code.sf.net/p/kactus2/code/trunk@1321 f0aa6db5-7f46-43cc-a8a5-a31efdaafa67

---
+++ IPXactWrapper/ComponentEditor/ports/PortsView.cpp
@@ -0,0 +1,116 @@
+//-----------------------------------------------------------------------------
+// File: PortsView.cpp
+//-----------------------------------------------------------------------------
+// Project: Kactus 2
+// Author: Esko Pekkarinen
+// Date: 2.12.2013
+//
+// Description:
+// View for port editor.
+//-----------------------------------------------------------------------------
+
+#include "PortsView.h"
+#include "portsdelegate.h"
+
+#include <QMenu>
+
+//-----------------------------------------------------------------------------
+// Function: PortsView::PortsView()
+//-----------------------------------------------------------------------------
+PortsView::PortsView(QWidget *parent ):
+    EditableTableView(parent),
+    createBus_(tr("Create new bus definition"), this),
+    createExistingBus_(tr("Use existing bus definition"),this)
+{
+    connect(&createBus_, SIGNAL(triggered()),
+        this, SLOT(onCreateNewBus()), Qt::UniqueConnection);
+
+    connect(&createExistingBus_, SIGNAL(triggered()),
+        this, SLOT(onCreateExistingBus()), Qt::UniqueConnection);
+}
+
+//-----------------------------------------------------------------------------
+// Function: PortsView::~PortsView()
+//-----------------------------------------------------------------------------
+PortsView::~PortsView() 
+{
+}
+
+//-----------------------------------------------------------------------------
+// Function: PortsView::onCreateBus()
+//-----------------------------------------------------------------------------
+void PortsView::onCreateNewBus()
+{
+    QStringList selectedPorts;
+    foreach(QModelIndex portIndex, selectedIndexes())
+    {   
+
+        QString portName =  portIndex.sibling(portIndex.row(), PORT_COL_NAME).data().toString();
+        if (!selectedPorts.contains(portName))
+        {
+            selectedPorts.append(portName);
+        }
+    }
+
+    emit createBus(selectedPorts);
+}
+
+//-----------------------------------------------------------------------------
+// Function: PortsView::onCreateExistingBus()
+//-----------------------------------------------------------------------------
+void PortsView::onCreateExistingBus()
+{
+    QStringList selectedPorts;
+    foreach(QModelIndex portIndex, selectedIndexes())
+    {   
+
+        QString portName =  portIndex.sibling(portIndex.row(), PORT_COL_NAME).data().toString();
+        if (!selectedPorts.contains(portName))
+        {
+            selectedPorts.append(portName);
+        }
+    }
+
+    emit createExistingBus(selectedPorts);
+}
+
+//-----------------------------------------------------------------------------
+// Function: PortsView::contextMenuEvent()
+//-----------------------------------------------------------------------------
+void PortsView::contextMenuEvent(QContextMenuEvent* event)
+{
+    pressedPoint_ = event->pos();
+
+    QModelIndex index = indexAt(pressedPoint_);
+
+    QMenu menu(this);
+    menu.addAction(&addAction_);
+
+    // if at least one valid item is selected
+    if (index.isValid()) {
+        menu.addAction(&removeAction_);
+        menu.addAction(&clearAction_);
+        menu.addAction(&copyAction_);
+    }
+
+    menu.addAction(&pasteAction_);
+    
+    if (index.isValid())
+    {
+        QMenu* createMenu = menu.addMenu(tr("New bus interface"));
+        createMenu->addAction(&createBus_);    
+        createMenu->addAction(&createExistingBus_);           
+    }
+
+    if (impExportable_) {
+        menu.addAction(&importAction_);
+        menu.addAction(&exportAction_);
+    }
+
+    menu.exec(event->globalPos());
+
+    event->accept();
+}
+
+
+
---
+++ IPXactWrapper/ComponentEditor/ports/PortsView.h
@@ -0,0 +1,71 @@
+//-----------------------------------------------------------------------------
+// File: PortsView.h
+//-----------------------------------------------------------------------------
+// Project: Kactus 2
+// Author: Esko Pekkarinen
+// Date: 2.12.2013
+//
+// Description:
+// View for port editor.
+//-----------------------------------------------------------------------------
+#ifndef PortsView_H
+#define PortsView_H
+
+#include <common/views/EditableTableView/editabletableview.h>
+#include <models/port.h>
+#include <models/component.h>
+
+#include <QAction>
+
+//-----------------------------------------------------------------------------
+// View for port editor.
+//-----------------------------------------------------------------------------
+class PortsView : public EditableTableView {
+	Q_OBJECT
+
+public:
+
+
+	/*!
+	 *  The constructor.
+	 *
+	 *      @param [in] parent   The parent widget.	 
+	 */
+	PortsView(QWidget *parent);
+
+	/*!
+	 *  The destructor.
+	 */
+	virtual ~PortsView();
+
+signals:
+
+    //! Emitted when new bus interface is selected from the context menu and a new bus definition must be created.
+    void createBus(QStringList const& portNames);
+
+    //! Emitted when new bus interface is selected from the context menu.
+    void createExistingBus(QStringList const& portNames);
+
+private slots:
+
+    //! Handler for new bus definition and interface creation.     
+    virtual void onCreateNewBus();
+
+    //! Handler for new bus interface creation.
+    virtual void onCreateExistingBus();
+
+protected:
+
+    //! Handler for context menu events.
+    virtual void contextMenuEvent(QContextMenuEvent* event);
+
+private:
+
+    //! Action for creating a new bus definition and interface.
+    QAction createBus_;
+
+    //! Action for creating a new bus interface.
+    QAction createExistingBus_;
+};
+
+#endif // PortsView_H
