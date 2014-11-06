518ba5a7eecbe64e872f6c0df933a8b18d5fb52b
Esko Pekkarinen <esko.pekkarinen@tut.fi>
2014-11-06 14:40:35 +0000

    [PERFECTIVE] Updated copyrights.
    git-svn-id: svn://svn.code.sf.net/p/kactus2/code/trunk@1653 f0aa6db5-7f46-43cc-a8a5-a31efdaafa67

--- mainwindow/SplashScreen.cpp
+++ mainwindow/SplashScreen.cpp
@@ -63,7 +63,7 @@ void SplashScreen::drawContents(QPainter *painter)
     painter->setFont(font);    
  
     painter->drawText(QRectF(5, 240, 590, 160), Qt::AlignCenter,
-        QString::fromLatin1("Copyright 2010-2013 Tampere University of Technology,\n" 
+        QString::fromLatin1("Copyright 2010-2014 Tampere University of Technology,\n" 
                             "Korkeakoulunkatu 10, FI-33720 Tampere, Finland\n"
                             "http://funbase.cs.tut.fi\n"
                             "kactus2@cs.tut.fi\n" 
