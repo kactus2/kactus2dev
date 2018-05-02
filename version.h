4dda704668b9ec38a9eaa7d9a719c31dcd6c735b
Esko <esko.pekkarinen@tut.fi>
2018-05-02 10:09:01 +0300

    [CORRECTIVE] Fixed locating help files for local install settings in linux (refs #356)

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           3.5.861.0
+#define VERSION_FULL           3.5.862.0
 
 #define VERSION_BASEYEAR       0
 #define VERSION_DATE           "2018-05-02"
-#define VERSION_TIME           "08:17:53"
+#define VERSION_TIME           "10:07:23"
 
 #define VERSION_MAJOR          3
 #define VERSION_MINOR          5
-#define VERSION_BUILDNO        861
+#define VERSION_BUILDNO        862
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           3,5,861,0
-#define VERSION_PRODUCT        3,5,861,0
-#define VERSION_FILESTR        "3,5,861,0"
-#define VERSION_PRODUCTSTR     "3,5,861,0"
+#define VERSION_FILE           3,5,862,0
+#define VERSION_PRODUCT        3,5,862,0
+#define VERSION_FILESTR        "3,5,862,0"
+#define VERSION_PRODUCTSTR     "3,5,862,0"
 
 #endif
