31753dc6ff7c7d06ed68919b34ebb055438d5629
Esko <esko.pekkarinen@tut.fi>
2018-05-03 11:24:18 +0300

    [PERFECTIVE] Improved linux installation for user-selected target directory (refs #356).

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           3.5.862.0
+#define VERSION_FULL           3.5.864.0
 
 #define VERSION_BASEYEAR       0
-#define VERSION_DATE           "2018-05-02"
-#define VERSION_TIME           "10:07:23"
+#define VERSION_DATE           "2018-05-03"
+#define VERSION_TIME           "10:23:44"
 
 #define VERSION_MAJOR          3
 #define VERSION_MINOR          5
-#define VERSION_BUILDNO        862
+#define VERSION_BUILDNO        864
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           3,5,862,0
-#define VERSION_PRODUCT        3,5,862,0
-#define VERSION_FILESTR        "3,5,862,0"
-#define VERSION_PRODUCTSTR     "3,5,862,0"
+#define VERSION_FILE           3,5,864,0
+#define VERSION_PRODUCT        3,5,864,0
+#define VERSION_FILESTR        "3,5,864,0"
+#define VERSION_PRODUCTSTR     "3,5,864,0"
 
 #endif
