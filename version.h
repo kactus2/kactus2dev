4d571c44e63a0672ff0bacba6319bd3375897f70
epekkar <esko.pekkarinen@tuni.fi>
2019-09-06 12:31:30 +0300

    [CORRECTIVE] Fixed an issue in "Save As..." on locked bus definition.

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           3.7.541.0
+#define VERSION_FULL           3.7.547.0
 
 #define VERSION_BASEYEAR       0
-#define VERSION_DATE           "2019-08-26"
-#define VERSION_TIME           "13:16:30"
+#define VERSION_DATE           "2019-09-06"
+#define VERSION_TIME           "12:25:52"
 
 #define VERSION_MAJOR          3
 #define VERSION_MINOR          7
-#define VERSION_BUILDNO        541
+#define VERSION_BUILDNO        547
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           3,7,541,0
-#define VERSION_PRODUCT        3,7,541,0
-#define VERSION_FILESTR        "3,7,541,0"
-#define VERSION_PRODUCTSTR     "3,7,541,0"
+#define VERSION_FILE           3,7,547,0
+#define VERSION_PRODUCT        3,7,547,0
+#define VERSION_FILESTR        "3,7,547,0"
+#define VERSION_PRODUCTSTR     "3,7,547,0"
 
 #endif
