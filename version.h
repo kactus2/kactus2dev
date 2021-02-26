5ddb96f43866bb501fbed729b221ae42613726f7
epekkar <esko.pekkarinen@tuni.fi>
2021-02-26 09:56:16 +0200

    [PERFECTIVE] Fixed VS project files for Windows build.

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           3.8.575.0
+#define VERSION_FULL           3.8.577.0
 
 #define VERSION_BASEYEAR       0
 #define VERSION_DATE           "2021-02-26"
-#define VERSION_TIME           "05:29:40"
+#define VERSION_TIME           "09:39:10"
 
 #define VERSION_MAJOR          3
 #define VERSION_MINOR          8
-#define VERSION_BUILDNO        575
+#define VERSION_BUILDNO        577
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           3,8,575,0
-#define VERSION_PRODUCT        3,8,575,0
-#define VERSION_FILESTR        "3,8,575,0"
-#define VERSION_PRODUCTSTR     "3,8,575,0"
+#define VERSION_FILE           3,8,577,0
+#define VERSION_PRODUCT        3,8,577,0
+#define VERSION_FILESTR        "3,8,577,0"
+#define VERSION_PRODUCTSTR     "3,8,577,0"
 
 #endif
