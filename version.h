8f091313deb604777c1b9e72f8599f11296b4eac
epekkar <esko.pekkarinen@tuni.fi>
2021-03-19 08:17:49 +0200

    [CORRECTIVE] Fixed Qt version setup for VHDLSourceAnalyzer.

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           3.8.465.0
+#define VERSION_FULL           3.8.466.0
 
 #define VERSION_BASEYEAR       0
 #define VERSION_DATE           "2021-03-15"
-#define VERSION_TIME           "11:22:26"
+#define VERSION_TIME           "13:49:49"
 
 #define VERSION_MAJOR          3
 #define VERSION_MINOR          8
-#define VERSION_BUILDNO        465
+#define VERSION_BUILDNO        466
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           3,8,465,0
-#define VERSION_PRODUCT        3,8,465,0
-#define VERSION_FILESTR        "3,8,465,0"
-#define VERSION_PRODUCTSTR     "3,8,465,0"
+#define VERSION_FILE           3,8,466,0
+#define VERSION_PRODUCT        3,8,466,0
+#define VERSION_FILESTR        "3,8,466,0"
+#define VERSION_PRODUCTSTR     "3,8,466,0"
 
 #endif
