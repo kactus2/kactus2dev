e9f223c34bbb55aa62439aa6cf4dd57a0c34d31c
teuhom <mikko.teuho@tuni.fi>
2021-03-29 14:28:58 +0300

    [CORRECTIVE] Updated resets interface.

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           3.8.577.0
+#define VERSION_FULL           3.8.579.0
 
 #define VERSION_BASEYEAR       0
-#define VERSION_DATE           "2021-03-26"
-#define VERSION_TIME           "13:34:04"
+#define VERSION_DATE           "2021-03-29"
+#define VERSION_TIME           "14:24:47"
 
 #define VERSION_MAJOR          3
 #define VERSION_MINOR          8
-#define VERSION_BUILDNO        577
+#define VERSION_BUILDNO        579
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           3,8,577,0
-#define VERSION_PRODUCT        3,8,577,0
-#define VERSION_FILESTR        "3,8,577,0"
-#define VERSION_PRODUCTSTR     "3,8,577,0"
+#define VERSION_FILE           3,8,579,0
+#define VERSION_PRODUCT        3,8,579,0
+#define VERSION_FILESTR        "3,8,579,0"
+#define VERSION_PRODUCTSTR     "3,8,579,0"
 
 #endif
