1808529cb891ebe1053d4773b84329b78fd14c7e
teuhom <mikko.teuho@tuni.fi>
2021-08-12 10:40:09 +0300

    [ADDITIVE] Added SVD generation to Python interpreter.

--- Plugins/SVDGenerator/ConnectivityGraphUtilities.cpp
+++ Plugins/SVDGenerator/ConnectivityGraphUtilities.cpp
@@ -11,6 +11,8 @@
 
 #include "ConnectivityGraphUtilities.h"
 
+#include <editors/MemoryDesigner/ConnectivityGraphFactory.h>
+#include <editors/MemoryDesigner/MasterSlavePathSearch.h>
 #include <editors/MemoryDesigner/MemoryItem.h>
 #include <editors/MemoryDesigner/MemoryDesignerConstants.h>
 
@@ -58,4 +60,98 @@ QSharedPointer<Cpu> ConnectivityGraphUtilities::getInterfacedCPU(QSharedPointer<
     }
 
     return QSharedPointer<Cpu>();
+}
+
+//-----------------------------------------------------------------------------
+// Function: ConnectivityGraphUtilities::getDefaultCPUs()
+//-----------------------------------------------------------------------------
+QVector<QSharedPointer<ConnectivityGraphUtilities::cpuDetailRoutes> > ConnectivityGraphUtilities::getDefaultCPUs(
+    LibraryInterface* library, QSharedPointer<Component> component, QString const& viewName)
+{
+    QVector<QSharedPointer<ConnectivityGraphUtilities::cpuDetailRoutes> > defaultCPUs;
+
+    ConnectivityGraphFactory graphFactory(library);
+    QSharedPointer<ConnectivityGraph> graph = graphFactory.createConnectivityGraph(component, viewName);
+    MasterSlavePathSearch searchAlgorithm;
+    QVector<QVector<QSharedPointer<const ConnectivityInterface>>> masterRoutes =
+        searchAlgorithm.findMasterSlavePaths(graph);
+
+    for (auto masterRoute : masterRoutes)
+    {
+        QSharedPointer<const ConnectivityInterface> master = masterRoute.first();
+        if (interfacedCpuExists(master, defaultCPUs))
+        {
+            QSharedPointer<ConnectivityGraphUtilities::cpuDetailRoutes> checkInterface =
+                getMatchingCpuContainer(master, defaultCPUs);
+            checkInterface->routes_.append(masterRoute);
+        }
+        else
+        {
+            QSharedPointer<const ConnectivityComponent> routeComponent = master->getInstance();
+            QSharedPointer<const Component> interfaceComponent =
+                ConnectivityGraphUtilities::getInterfacedComponent(library, routeComponent);
+            if (interfaceComponent)
+            {
+                QSharedPointer<Cpu> interfaceCPU =
+                    ConnectivityGraphUtilities::getInterfacedCPU(interfaceComponent, master);
+                if (interfaceCPU)
+                {
+                    QString checkBoxText = interfaceCPU->name() + " [" + routeComponent->getName() + "]";
+                    QSharedPointer<ConnectivityGraphUtilities::cpuDetailRoutes> cpuInterface(
+                        new ConnectivityGraphUtilities::cpuDetailRoutes());
+                    cpuInterface->cpuName_ = checkBoxText;
+                    cpuInterface->cpuID_ = checkBoxText;
+                    cpuInterface->cpuInterface_ = master;
+                    cpuInterface->routes_.append(masterRoute);
+
+                    cpuInterface->createFile_ = true;
+                    cpuInterface->revision_ = QString();
+                    cpuInterface->endian_ = QString();
+                    cpuInterface->mpuPresent_ = false;
+                    cpuInterface->fpuPresent_ = false;
+                    cpuInterface->nvicPrioBits_ = QString();
+                    cpuInterface->vendorSystickConfig_ = false;
+
+                    defaultCPUs.append(cpuInterface);
+                }
+            }
+        }
+    }
+
+    return defaultCPUs;
+}
+
+//-----------------------------------------------------------------------------
+// Function: ConnectivityGraphUtilities::interfacedCpuExists()
+//-----------------------------------------------------------------------------
+bool ConnectivityGraphUtilities::interfacedCpuExists(QSharedPointer<const ConnectivityInterface> master,
+    QVector<QSharedPointer<ConnectivityGraphUtilities::cpuDetailRoutes>> cpuList)
+{
+    for (auto checkInterface : cpuList)
+    {
+        if (checkInterface->cpuInterface_ == master)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+//-----------------------------------------------------------------------------
+// Function: ConnectivityGraphUtilities::getMatchingCheckInterface()
+//-----------------------------------------------------------------------------
+QSharedPointer<ConnectivityGraphUtilities::cpuDetailRoutes> ConnectivityGraphUtilities::getMatchingCpuContainer(
+    QSharedPointer<const ConnectivityInterface> master,
+    QVector<QSharedPointer<ConnectivityGraphUtilities::cpuDetailRoutes>> cpuList)
+{
+    for (auto checkInterface : cpuList)
+    {
+        if (checkInterface->cpuInterface_ == master)
+        {
+            return checkInterface;
+        }
+    }
+
+    return QSharedPointer<ConnectivityGraphUtilities::cpuDetailRoutes>();
 }
--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           3.9.0.0
+#define VERSION_FULL           3.9.5.0
 
 #define VERSION_BASEYEAR       0
-#define VERSION_DATE           "2021-06-24"
-#define VERSION_TIME           "09:49:41"
+#define VERSION_DATE           "2021-08-12"
+#define VERSION_TIME           "10:38:04"
 
 #define VERSION_MAJOR          3
 #define VERSION_MINOR          9
-#define VERSION_BUILDNO        0
+#define VERSION_BUILDNO        5
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           3,9,0,0
-#define VERSION_PRODUCT        3,9,0,0
-#define VERSION_FILESTR        "3,9,0,0"
-#define VERSION_PRODUCTSTR     "3,9,0,0"
+#define VERSION_FILE           3,9,5,0
+#define VERSION_PRODUCT        3,9,5,0
+#define VERSION_FILESTR        "3,9,5,0"
+#define VERSION_PRODUCTSTR     "3,9,5,0"
 
 #endif
