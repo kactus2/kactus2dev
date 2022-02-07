8925c94cd3829e51220ceb30809711f39329c519
epekkar <esko.pekkarinen@tuni.fi>
2022-02-07 14:13:43 +0200

    [CORRECTIVE] Fixed issue where importing VHDL generics would stop on semicolon in comments.

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           3.9.171.0
+#define VERSION_FULL           3.9.172.0
 
 #define VERSION_BASEYEAR       0
-#define VERSION_DATE           "2022-01-20"
-#define VERSION_TIME           "09:20:45"
+#define VERSION_DATE           "2022-02-07"
+#define VERSION_TIME           "13:16:47"
 
 #define VERSION_MAJOR          3
 #define VERSION_MINOR          9
-#define VERSION_BUILDNO        171
+#define VERSION_BUILDNO        172
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           3,9,171,0
-#define VERSION_PRODUCT        3,9,171,0
-#define VERSION_FILESTR        "3,9,171,0"
-#define VERSION_PRODUCTSTR     "3,9,171,0"
+#define VERSION_FILE           3,9,172,0
+#define VERSION_PRODUCT        3,9,172,0
+#define VERSION_FILESTR        "3,9,172,0"
+#define VERSION_PRODUCTSTR     "3,9,172,0"
 
 #endif
