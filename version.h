6806679b4e9ae6d287f7748e966006ff2f2f6cb4
epekkar <esko.pekkarinen@tuni.fi>
2022-03-10 08:23:33 +0200

    [ADDITIVE] Added help page for subspace maps and added missing images for indirect interfaces help page.

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           3.9.324.0
+#define VERSION_FULL           3.9.327.0
 
 #define VERSION_BASEYEAR       0
-#define VERSION_DATE           "2022-03-09"
-#define VERSION_TIME           "09:24:17"
+#define VERSION_DATE           "2022-03-10"
+#define VERSION_TIME           "08:19:39"
 
 #define VERSION_MAJOR          3
 #define VERSION_MINOR          9
-#define VERSION_BUILDNO        324
+#define VERSION_BUILDNO        327
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           3,9,324,0
-#define VERSION_PRODUCT        3,9,324,0
-#define VERSION_FILESTR        "3,9,324,0"
-#define VERSION_PRODUCTSTR     "3,9,324,0"
+#define VERSION_FILE           3,9,327,0
+#define VERSION_PRODUCT        3,9,327,0
+#define VERSION_FILESTR        "3,9,327,0"
+#define VERSION_PRODUCTSTR     "3,9,327,0"
 
 #endif
