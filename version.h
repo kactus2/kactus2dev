78237a3582a832bcf19d3a82f39ebb237f146909
epekkar <esko.pekkarinen@tuni.fi>
2022-06-13 06:22:21 +0300

    [PERFECTIVE] Switch icons for Python script run and run all

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           3.9.568.0
+#define VERSION_FULL           3.9.569.0
 
 #define VERSION_BASEYEAR       0
-#define VERSION_DATE           "2022-06-10"
-#define VERSION_TIME           "14:21:09"
+#define VERSION_DATE           "2022-06-13"
+#define VERSION_TIME           "06:20:31"
 
 #define VERSION_MAJOR          3
 #define VERSION_MINOR          9
-#define VERSION_BUILDNO        568
+#define VERSION_BUILDNO        569
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           3,9,568,0
-#define VERSION_PRODUCT        3,9,568,0
-#define VERSION_FILESTR        "3,9,568,0"
-#define VERSION_PRODUCTSTR     "3,9,568,0"
+#define VERSION_FILE           3,9,569,0
+#define VERSION_PRODUCT        3,9,569,0
+#define VERSION_FILESTR        "3,9,569,0"
+#define VERSION_PRODUCTSTR     "3,9,569,0"
 
 #endif
