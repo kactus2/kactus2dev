f8da5137d3b761429ddb870c9724ea84988075e4
epekkar <esko.pekkarinen@tuni.fi>
2022-06-17 10:23:47 +0300

    [MIGRATION] Update solution to VS2022

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           3.9.618.0
+#define VERSION_FULL           3.9.620.0
 
 #define VERSION_BASEYEAR       0
-#define VERSION_DATE           "2022-06-16"
-#define VERSION_TIME           "10:02:30"
+#define VERSION_DATE           "2022-06-17"
+#define VERSION_TIME           "10:12:11"
 
 #define VERSION_MAJOR          3
 #define VERSION_MINOR          9
-#define VERSION_BUILDNO        618
+#define VERSION_BUILDNO        620
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           3,9,618,0
-#define VERSION_PRODUCT        3,9,618,0
-#define VERSION_FILESTR        "3,9,618,0"
-#define VERSION_PRODUCTSTR     "3,9,618,0"
+#define VERSION_FILE           3,9,620,0
+#define VERSION_PRODUCT        3,9,620,0
+#define VERSION_FILESTR        "3,9,620,0"
+#define VERSION_PRODUCTSTR     "3,9,620,0"
 
 #endif
