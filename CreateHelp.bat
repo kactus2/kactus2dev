cd357d326c6dd4c9b08d739bfe67de1365543b58
epekkar <esko.pekkarinen@tuni.fi>
2022-07-27 11:29:19 +0300

    [CORRECTIVE] Fix missing help content

--- CreateHelp.bat
+++ CreateHelp.bat
@@ -1,11 +1,11 @@
 REM
 REM Bind HTML pages to context helps. If you modify help texts, you
 REM don't need recompilation, if you run this script.
-REM Joni-Matti Määttä, Aug 2012
+REM Joni-Matti Mï¿½ï¿½ttï¿½, Aug 2012
 REM
 @echo off
 qhelpgenerator Help/kactus2help.qhp -o Help/Kactus2Help.qch
-qcollectiongenerator Help/kactus2help.qhcp -o Help/Kactus2Help.qhc
+qhelpgenerator Help/kactus2help.qhcp -o Help/Kactus2Help.qhc
 xcopy /Y Help\Kactus2Help.qhc executable\Help\
 xcopy /Y Help\Kactus2Help.qch executable\Help\
 xcopy /Y Help\Kactus2Help.qhc x64\executable\Help\
