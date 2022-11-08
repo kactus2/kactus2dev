919fde5b8e7c0c9726f64f39460b5523169757e2
epekkar <esko.pekkarinen@tuni.fi>
2022-11-08 10:45:25 +0200

    [ADDITIVE] Add register dimensions for memory items and SVD generation.

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           3.10.68.0
+#define VERSION_FULL           3.10.75.0
 
 #define VERSION_BASEYEAR       0
-#define VERSION_DATE           "2022-10-13"
-#define VERSION_TIME           "09:38:39"
+#define VERSION_DATE           "2022-11-01"
+#define VERSION_TIME           "12:38:49"
 
 #define VERSION_MAJOR          3
 #define VERSION_MINOR          10
-#define VERSION_BUILDNO        68
+#define VERSION_BUILDNO        75
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           3,10,68,0
-#define VERSION_PRODUCT        3,10,68,0
-#define VERSION_FILESTR        "3,10,68,0"
-#define VERSION_PRODUCTSTR     "3,10,68,0"
+#define VERSION_FILE           3,10,75,0
+#define VERSION_PRODUCT        3,10,75,0
+#define VERSION_FILESTR        "3,10,75,0"
+#define VERSION_PRODUCTSTR     "3,10,75,0"
 
 #endif
