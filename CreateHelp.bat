643e8d7f62f51c508bae9311893bd1c750c51d7d
Esko Pekkarinen <esko.pekkarinen@tuni.fi>
2023-01-11 09:03:42 +0200

    [PERFECTIVE] Merge compressed help and help collection generation.

--- CreateHelp.bat
+++ CreateHelp.bat
@@ -4,7 +4,6 @@ REM don't need recompilation, if you run this script.
 REM Joni-Matti M��tt�, Aug 2012
 REM
 @echo off
-qhelpgenerator Help/kactus2help.qhp -o Help/Kactus2Help.qch
 qhelpgenerator Help/kactus2help.qhcp -o Help/Kactus2Help.qhc
 xcopy /Y Help\Kactus2Help.qhc executable\Help\
 xcopy /Y Help\Kactus2Help.qch executable\Help\
