076c8014ffa279d9052b4111966066a487f33c42
teuhom <mikko.teuho@tuni.fi>
2023-05-17 11:00:54 +0300

    [CORRECTIVE] Updated file structure.

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           3.11.432.0
+#define VERSION_FULL           3.11.433.0
 
 #define VERSION_BASEYEAR       0
-#define VERSION_DATE           "2023-05-10"
-#define VERSION_TIME           "15:06:48"
+#define VERSION_DATE           "2023-05-17"
+#define VERSION_TIME           "10:58:04"
 
 #define VERSION_MAJOR          3
 #define VERSION_MINOR          11
-#define VERSION_BUILDNO        432
+#define VERSION_BUILDNO        433
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           3,11,432,0
-#define VERSION_PRODUCT        3,11,432,0
-#define VERSION_FILESTR        "3,11,432,0"
-#define VERSION_PRODUCTSTR     "3,11,432,0"
+#define VERSION_FILE           3,11,433,0
+#define VERSION_PRODUCT        3,11,433,0
+#define VERSION_FILESTR        "3,11,433,0"
+#define VERSION_PRODUCTSTR     "3,11,433,0"
 
 #endif
