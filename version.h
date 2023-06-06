ef2219c91d97504fea53b3065a04aea5f49f1df9
epekkar <esko.pekkarinen@tuni.fi>
2023-06-06 14:54:24 +0300

    [PERFECTIVE] Update files for release

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           3.11.542.0
+#define VERSION_FULL           3.12.0.0
 
 #define VERSION_BASEYEAR       0
 #define VERSION_DATE           "2023-06-06"
-#define VERSION_TIME           "13:44:52"
+#define VERSION_TIME           "13:57:52"
 
 #define VERSION_MAJOR          3
-#define VERSION_MINOR          11
-#define VERSION_BUILDNO        542
+#define VERSION_MINOR          12
+#define VERSION_BUILDNO        0
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           3,11,542,0
-#define VERSION_PRODUCT        3,11,542,0
-#define VERSION_FILESTR        "3,11,542,0"
-#define VERSION_PRODUCTSTR     "3,11,542,0"
+#define VERSION_FILE           3,12,0,0
+#define VERSION_PRODUCT        3,12,0,0
+#define VERSION_FILESTR        "3,12,0,0"
+#define VERSION_PRODUCTSTR     "3,12,0,0"
 
 #endif
