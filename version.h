298c3c6fa6b691d3d768b68670f2755a86b26731
hagantsa <antsihag@gmail.com>
2023-06-08 09:31:37 +0300

    [ADDITIVE] Update ipxactmodels vs project files

--- version.h
+++ version.h
@@ -10,19 +10,19 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           3.11.447.0
+#define VERSION_FULL           3.11.537.0
 
 #define VERSION_BASEYEAR       0
-#define VERSION_DATE           "2023-05-19"
-#define VERSION_TIME           "15:50:29"
+#define VERSION_DATE           "2023-06-08"
+#define VERSION_TIME           "09:09:20"
 
 #define VERSION_MAJOR          3
 #define VERSION_MINOR          11
-#define VERSION_BUILDNO        447
+#define VERSION_BUILDNO        537
 #define VERSION_EXTEND         0
-#define VERSION_FILE           3,11,447,0
-#define VERSION_PRODUCT        3,11,447,0
-#define VERSION_FILESTR        "3,11,447,0"
-#define VERSION_PRODUCTSTR     "3,11,447,0"
+#define VERSION_FILE           3,11,537,0
+#define VERSION_PRODUCT        3,11,537,0
+#define VERSION_FILESTR        "3,11,537,0"
+#define VERSION_PRODUCTSTR     "3,11,537,0"
 
 #endif
