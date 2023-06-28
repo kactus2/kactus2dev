49e902421505d16776974c4a7afd6bf5a0c6b4ad
hagantsa <antsihag@gmail.com>
2023-06-28 10:20:25 +0300

    [ADDITIVE] Add placeholder description for extending bus def in new std editor.

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           3.12.312.0
+#define VERSION_FULL           3.12.314.0
 
 #define VERSION_BASEYEAR       0
 #define VERSION_DATE           "2023-06-28"
-#define VERSION_TIME           "09:45:18"
+#define VERSION_TIME           "10:14:43"
 
 #define VERSION_MAJOR          3
 #define VERSION_MINOR          12
-#define VERSION_BUILDNO        312
+#define VERSION_BUILDNO        314
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           3,12,312,0
-#define VERSION_PRODUCT        3,12,312,0
-#define VERSION_FILESTR        "3,12,312,0"
-#define VERSION_PRODUCTSTR     "3,12,312,0"
+#define VERSION_FILE           3,12,314,0
+#define VERSION_PRODUCT        3,12,314,0
+#define VERSION_FILESTR        "3,12,314,0"
+#define VERSION_PRODUCTSTR     "3,12,314,0"
 
 #endif
