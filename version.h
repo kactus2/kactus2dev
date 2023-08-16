082069c2a0b38abde4b3b4947a321277500a6a1f
hagantsa <antsihag@gmail.com>
2023-08-16 10:43:52 +0300

    [CORRECTIVE] Fix bus interface system mode reading.

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           3.12.545.0
+#define VERSION_FULL           3.12.546.0
 
 #define VERSION_BASEYEAR       0
-#define VERSION_DATE           "2023-08-15"
-#define VERSION_TIME           "16:29:56"
+#define VERSION_DATE           "2023-08-16"
+#define VERSION_TIME           "09:13:00"
 
 #define VERSION_MAJOR          3
 #define VERSION_MINOR          12
-#define VERSION_BUILDNO        545
+#define VERSION_BUILDNO        546
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           3,12,545,0
-#define VERSION_PRODUCT        3,12,545,0
-#define VERSION_FILESTR        "3,12,545,0"
-#define VERSION_PRODUCTSTR     "3,12,545,0"
+#define VERSION_FILE           3,12,546,0
+#define VERSION_PRODUCT        3,12,546,0
+#define VERSION_FILESTR        "3,12,546,0"
+#define VERSION_PRODUCTSTR     "3,12,546,0"
 
 #endif
