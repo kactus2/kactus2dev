d0a4700a348d1c7e86b113538291b67db3dae7c4
epekkar <esko.pekkarinen@tuni.fi>
2023-08-16 09:11:19 +0300

    [CORRECTIVE] Add missing files

---
+++ editors/ComponentEditor/treeStructure/SingleModeItem.cpp
@@ -0,0 +1,82 @@
+//-----------------------------------------------------------------------------
+// File: SingleModeItem.cpp
+//-----------------------------------------------------------------------------
+// Project: Kactus2
+// Author: Esko Pekkarinen
+// Date: 08.08.2023
+//
+// Description:
+// The single Mode item used in the component editor navigation tree.
+//-----------------------------------------------------------------------------
+
+#include "SingleModeItem.h"
+
+#include <editors/ComponentEditor/Modes/SingleModeEditor.h>
+#include <editors/common/ExpressionSet.h>
+
+#include <IPXACTmodels/Component/Mode.h>
+#include <IPXACTmodels/Component/validators/ModeValidator.h>
+
+//-----------------------------------------------------------------------------
+// Function: SingleModeItem::SingleModeItem()
+//-----------------------------------------------------------------------------
+SingleModeItem::SingleModeItem(QSharedPointer<Mode> mode, ComponentEditorTreeModel* model,
+    LibraryInterface* libHandler, QSharedPointer<Component> component,
+    QSharedPointer<ReferenceCounter> referenceCounter, ExpressionSet expressions,
+    QSharedPointer<ModeValidator> validator,
+    ComponentEditorItem* parent) :
+ComponentEditorItem(model, libHandler, component, parent),
+    mode_(mode),
+    validator_(validator),
+    expressions_(expressions)
+{
+    setParameterFinder(expressions_.finder);
+    setExpressionFormatter(expressions_.formatter);
+    setReferenceCounter(referenceCounter);
+}
+
+//-----------------------------------------------------------------------------
+// Function: SingleModeItem::getTooltip()
+//-----------------------------------------------------------------------------
+QString SingleModeItem::getTooltip() const
+{
+    return tr("Specifies a mode of the component");
+}
+
+//-----------------------------------------------------------------------------
+// Function: SingleModeItem::text()
+//-----------------------------------------------------------------------------
+QString SingleModeItem::text() const
+{
+    return mode_->name();
+}
+
+//-----------------------------------------------------------------------------
+// Function: SingleModeItem::isValid()
+//-----------------------------------------------------------------------------
+bool SingleModeItem::isValid() const
+{
+    return validator_->validate(mode_);
+}
+
+//-----------------------------------------------------------------------------
+// Function: SingleModeItem::editor()
+//-----------------------------------------------------------------------------
+ItemEditor* SingleModeItem::editor()
+{
+    if (!editor_)
+    {
+        editor_ = new SingleModeEditor(component_, mode_, libHandler_, expressions_);
+        editor_->setProtection(locked_);
+
+        connect(editor_, SIGNAL(contentChanged()), this, SLOT(onEditorChanged()), Qt::UniqueConnection);
+        connect(editor_, SIGNAL(errorMessage(const QString&)),
+            this, SIGNAL(errorMessage(const QString&)), Qt::UniqueConnection);
+        connect(editor_, SIGNAL(helpUrlRequested(QString const&)),
+            this, SIGNAL(helpUrlRequested(QString const&)), Qt::UniqueConnection);
+
+        connectItemEditorToReferenceCounter();
+    }
+
+    return editor_;
+}
---
+++ editors/ComponentEditor/treeStructure/SingleModeItem.h
@@ -0,0 +1,102 @@
+//-----------------------------------------------------------------------------
+// File: SingleModeItem.h
+//-----------------------------------------------------------------------------
+// Project: Kactus2
+// Author: Esko Pekkarinen
+// Date: 08.08.2023
+//
+// Description:
+// The single Mode item used in the component editor navigation tree.
+//-----------------------------------------------------------------------------
+
+#ifndef SINGLE_MODE_ITEM_H
+#define SINGLE_MODE_ITEM_H
+
+#include "componenteditoritem.h"
+
+#include <editors/common/ExpressionSet.h>
+
+class Mode;
+class ModeValidator;
+
+//-----------------------------------------------------------------------------
+//! The single Mode item used in the component editor navigation tree.
+//-----------------------------------------------------------------------------
+class SingleModeItem: public ComponentEditorItem
+{
+	Q_OBJECT
+
+public:
+	
+    /*!
+     *  The constructor.
+     *
+     *      @param [in] Mode                     The selected Mode.
+     *      @param [in] model                   The model that owns the items.
+     *      @param [in] libHandler              The instance that manages the library.
+     *      @param [in] component               The component being edited.
+     *      @param [in] referenceCounter        The counter for parameter references.
+     *      @param [in] expressions				The collection of objects for expression handling.
+     *      @param [in] parent                  The parent item.
+     */
+    SingleModeItem(QSharedPointer<Mode> mode,
+        ComponentEditorTreeModel* model,
+        LibraryInterface* libHandler,
+        QSharedPointer<Component> component,
+        QSharedPointer<ReferenceCounter> referenceCounter,
+        ExpressionSet expressions,
+        QSharedPointer<ModeValidator> validator,
+        ComponentEditorItem* parent);
+
+    /*!
+     *  The destructor.
+     */
+    virtual ~SingleModeItem() = default;
+
+    //! No copying
+    SingleModeItem(const SingleModeItem& other) = delete;
+
+    //! No assignment
+    SingleModeItem& operator=(const SingleModeItem& other) = delete;
+
+	/*!
+	 *  Get the tool tip for the item.
+	 *
+     *      @return The text for the tool tip to print to user.
+	 */
+	QString getTooltip() const final;
+
+	/*!
+	 *  Get the text to be displayed in the tree for this item.
+	 *
+     *      @return The text to display.
+	 */
+	QString text() const final;
+
+    /*!
+	 *  Check the validity of this item and sub items.
+	 *
+     *      @return True, if the item is in a valid state, false otherwise.
+	 */
+	bool isValid() const final;
+
+	/*!
+	 *  Get the pointer to the editor of this item.
+	 *
+     *      @return Pointer to the editor to use for this item.
+	 */
+	ItemEditor* editor() final;
+
+private:
+
+    QSharedPointer<ExpressionParser> expressionParser_;
+
+    //! Pointer to the currently selected remap state.
+    QSharedPointer<Mode> mode_;
+
+    QSharedPointer<ModeValidator> validator_;
+
+    ExpressionSet expressions_;
+};
+
+#endif // SINGLE_MODE_ITEM_H
