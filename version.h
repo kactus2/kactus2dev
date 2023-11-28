91ffa58039b9de664ba45eceb69f0d87f81d2a6a
teuhom <mikko.teuho@tuni.fi>
2023-11-28 11:59:22 +0200

    [PERFECTIVE] Use field interface when creating a new field for a new register.

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           3.12.1047.0
+#define VERSION_FULL           3.12.1056.0
 
 #define VERSION_BASEYEAR       0
-#define VERSION_DATE           "2023-11-22"
-#define VERSION_TIME           "11:33:25"
+#define VERSION_DATE           "2023-11-28"
+#define VERSION_TIME           "11:54:27"
 
 #define VERSION_MAJOR          3
 #define VERSION_MINOR          12
-#define VERSION_BUILDNO        1047
+#define VERSION_BUILDNO        1056
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           3,12,1047,0
-#define VERSION_PRODUCT        3,12,1047,0
-#define VERSION_FILESTR        "3,12,1047,0"
-#define VERSION_PRODUCTSTR     "3,12,1047,0"
+#define VERSION_FILE           3,12,1056,0
+#define VERSION_PRODUCT        3,12,1056,0
+#define VERSION_FILESTR        "3,12,1056,0"
+#define VERSION_PRODUCTSTR     "3,12,1056,0"
 
 #endif
