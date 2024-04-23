81c2b8c6f7947958c64b4495a900670b0d747e3a
hagantsa <anton.hagqvist@tuni.fi>
2024-04-23 16:04:30 +0300

    [ADDITIVE] Update release files

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           3.13.493.0
+#define VERSION_FULL           3.13.2.0
 
 #define VERSION_BASEYEAR       0
-#define VERSION_DATE           "2024-04-22"
-#define VERSION_TIME           "15:37:17"
+#define VERSION_DATE           "2024-04-23"
+#define VERSION_TIME           "12:42:17"
 
 #define VERSION_MAJOR          3
 #define VERSION_MINOR          13
-#define VERSION_BUILDNO        493
+#define VERSION_BUILDNO        2
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           3,13,493,0
-#define VERSION_PRODUCT        3,13,493,0
-#define VERSION_FILESTR        "3,13,493,0"
-#define VERSION_PRODUCTSTR     "3,13,493,0"
+#define VERSION_FILE           3,13,2,0
+#define VERSION_PRODUCT        3,13,2,0
+#define VERSION_FILESTR        "3,13,2,0"
+#define VERSION_PRODUCTSTR     "3,13,2,0"
 
 #endif
