b80074e0e401b9f3053a5717988f13be5f10e77d
hagantsa <anton.hagqvist@tuni.fi>
2024-06-18 14:16:27 +0300

    [PERFECTIVE] Add spacer to subspace map definition editor to make it look nicer.

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           3.13.328.0
+#define VERSION_FULL           3.13.332.0
 
 #define VERSION_BASEYEAR       0
 #define VERSION_DATE           "2024-06-18"
-#define VERSION_TIME           "12:20:08"
+#define VERSION_TIME           "14:13:51"
 
 #define VERSION_MAJOR          3
 #define VERSION_MINOR          13
-#define VERSION_BUILDNO        328
+#define VERSION_BUILDNO        332
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           3,13,328,0
-#define VERSION_PRODUCT        3,13,328,0
-#define VERSION_FILESTR        "3,13,328,0"
-#define VERSION_PRODUCTSTR     "3,13,328,0"
+#define VERSION_FILE           3,13,332,0
+#define VERSION_PRODUCT        3,13,332,0
+#define VERSION_FILESTR        "3,13,332,0"
+#define VERSION_PRODUCTSTR     "3,13,332,0"
 
 #endif
