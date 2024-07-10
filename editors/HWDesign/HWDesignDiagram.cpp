kactus2/kactus2dev

commit fc14c8aa3d89f39061511510eb1ca9876e82dd8b

[CORRECTIVE] Fix loading port and bus interface changes made in component editor to design. [CORRECTIVE] Fix duplicate bus interface data appearing in design.

editors/HWDesign/HWDesignDiagram.cpp
@@ -246,10 +246,12 @@ void HWDesignDiagram::updateHierComponent()
     for (QGraphicsItem *item : items())
     {
         // Check if the item is a diagram interface and its bus interface is defined.
-        auto diagIf = dynamic_cast<HierarchicalBusInterfaceItem*>(item);
-        if (diagIf != nullptr && diagIf->getBusInterface() != nullptr && !diagIf->isInvalid())
+        if (auto diagIf = dynamic_cast<HierarchicalBusInterfaceItem*>(item))
         {
-			busIfs.append(diagIf->getBusInterface());
+            if (diagIf->getBusInterface() != nullptr && !diagIf->isInvalid())
+            {
+			    busIfs.append(diagIf->getBusInterface());
+            }
         }
     }
 
@@ -258,19 +260,36 @@ void HWDesignDiagram::updateHierComponent()
     // Add any new interfaces in the component.
     for (auto diagramInterface : busIfs)
     {
-        if (componentInterfaces->contains(diagramInterface) == false)
+        bool busExists = std::find_if(componentInterfaces->cbegin(), componentInterfaces->cend(), [&diagramInterface](auto componentBusIf)
+            {
+                return componentBusIf->name() == diagramInterface->name();
+            }) != componentInterfaces->cend();
+
+        if (!busExists)
         {
             componentInterfaces->append(diagramInterface);
         }
     }
 
+    QList<QSharedPointer<BusInterface> > toRemove;
+
     // Remove interfaces deleted in the design from the component.
     for (auto componentInterface : *componentInterfaces)
     {
-        if (busIfs.contains(componentInterface) == false)
+        bool busExists = std::find_if(busIfs.cbegin(), busIfs.cend(), [&componentInterface](auto diagramInterface)
+            {
+                return componentInterface->name() == diagramInterface->name();
+            }) != busIfs.cend();
+
+        if (!busExists)
         {
-            componentInterfaces->removeAll(componentInterface);
+            toRemove.append(componentInterface);
         }
+    }
+
+    for (auto interfaceToRemove : toRemove)
+    {
+        componentInterfaces->removeAll(interfaceToRemove);
     }
 }
 
@@ -366,6 +385,23 @@ HWConnectionEndpoint* HWDesignDiagram::getDiagramAdHocPort(QString const& portNa
     {
         if (item->type() == HierarchicalPortItem::Type &&
             static_cast<HierarchicalPortItem*>(item)->name().compare(portName) == 0)
+        {
+            return static_cast<HWConnectionEndpoint*>(item);
+        }
+    }
+
+    return nullptr;
+}
+
+//-----------------------------------------------------------------------------
+// Function: HWDesignDiagram::getHierarchicalInterface()
+//-----------------------------------------------------------------------------
+HWConnectionEndpoint* HWDesignDiagram::getHierarchicalInterface(QString const& busRef) const
+{
+    for (QGraphicsItem* item : items())
+    {
+        if (item->type() == HierarchicalBusInterfaceItem::Type &&
+            static_cast<HierarchicalBusInterfaceItem*>(item)->name().compare(busRef) == 0)
         {
             return static_cast<HWConnectionEndpoint*>(item);
         }
@@ -2308,8 +2344,24 @@ ConnectionEndpoint* HWDesignDiagram::findOrCreateHierarchicalInterface(QString c
         busIf = QSharedPointer<BusInterface>(new BusInterface());
         busIf->setName(busRef);
 
-        QSharedPointer<InterfaceGraphicsData> dataGroup(new InterfaceGraphicsData(busIf->name()));
-        getDesign()->getVendorExtensions()->append(dataGroup);
+        // Check if there already is data for interface with this name (interface was deleted or renamed)
+        auto designInterfaceGraphicsData = getDesign()->getInterfaceGraphicsData();
+        auto interfaceGraphicsDataIter = std::find_if(designInterfaceGraphicsData.cbegin(), designInterfaceGraphicsData.cend(),
+            [&busIf](QSharedPointer<InterfaceGraphicsData> graphicsData)
+            {
+                return graphicsData->getName() == busIf->name();
+            });
+
+        QSharedPointer<InterfaceGraphicsData> dataGroup;
+        if (interfaceGraphicsDataIter != designInterfaceGraphicsData.cend())
+        {
+            dataGroup = *interfaceGraphicsDataIter;
+        }
+        else
+        {
+            dataGroup = QSharedPointer<InterfaceGraphicsData>(new InterfaceGraphicsData(busIf->name()));
+            getDesign()->getVendorExtensions()->append(dataGroup);
+        }
 
         auto hierarchicalInterface = new HierarchicalBusInterfaceItem(getEditedComponent(), busIf, dataGroup, nullptr);
         hierarchicalInterface->setTemporary(true);

editors/HWDesign/HWDesignDiagram.h
@@ -238,6 +238,16 @@ public:
      */
     virtual HWConnectionEndpoint* getDiagramAdHocPort(QString const& portName);
 
+    /*!
+     *	Returns the bus interface item of the corresponding design component bus interface with the given name 
+     *  or nullptr if not found.
+     *  
+     *      @param [in] busRef     The interface name.
+     *	    
+     * 	    @return The hierarchical bus interface item found or null.
+     */
+    HWConnectionEndpoint* getHierarchicalInterface(QString const& busRef) const;
+
 public slots:
 
 	/*!

editors/HWDesign/HWDesignWidget.cpp
@@ -884,6 +884,137 @@ void HWDesignWidget::updateFiles(QSharedPointer<Component> topComponent, QString
 }
 
 //-----------------------------------------------------------------------------
+// Function: HWDesignWidget::loadChangesFromSibling()
+//-----------------------------------------------------------------------------
+void HWDesignWidget::loadChangesFromRelatedTab()
+{
+    // Load updated component from disk
+    if (auto libraryComponent = getLibraryInterface()->getModelReadOnly<Component>(getEditedComponent()->getVlnv()))
+    {
+        loadBusInterfaceChanges(libraryComponent);
+        loadPortChanges(libraryComponent);
+    }
+}
+
+//-----------------------------------------------------------------------------
+// Function: HWDesignWidget::loadBusInterfaceChanges()
+//-----------------------------------------------------------------------------
+void HWDesignWidget::loadBusInterfaceChanges(QSharedPointer<Component const> libraryComponent)
+{
+    auto design = getDiagram()->getDesign();
+    auto designComponentInterfaces = QList<QSharedPointer<BusInterface> >(*getEditedComponent()->getBusInterfaces());
+    auto libraryComponentInterfaces = QList<QSharedPointer<BusInterface> >(*libraryComponent->getBusInterfaces());
+
+    // First check for added interfaces
+    for (auto const& compBusif : libraryComponentInterfaces)
+    {
+        // Replace if found with same name
+        if (auto foundInterface = Search::findByName(compBusif->name(), designComponentInterfaces))
+        {
+            designComponentInterfaces.removeOne(foundInterface);
+        }
+
+        designComponentInterfaces.append(compBusif);
+    }
+
+    // Remove deleted interfaces (aka if interface exists in design component but not in component loaded from disk)
+    for (auto designComponentInterfaceIt = designComponentInterfaces.begin(); designComponentInterfaceIt != designComponentInterfaces.end();)
+    {
+        if (auto foundInterface = Search::findByName((*designComponentInterfaceIt)->name(), libraryComponentInterfaces);
+            foundInterface == nullptr)
+        {
+            // Remove connections and routes from design, if interface had no connections
+            auto hwDiagram = static_cast<HWDesignDiagram*>(getDiagram());
+            if (auto endpointItem = hwDiagram->getHierarchicalInterface((*designComponentInterfaceIt)->name()))
+            {
+                auto busItem = static_cast<HierarchicalBusInterfaceItem*>(endpointItem);
+
+                if (auto const& connections = busItem->getConnections(); connections.isEmpty())
+                {
+                    design->removeInterfaceGraphicsData((*designComponentInterfaceIt)->name());
+                    for (auto const& connection : busItem->getConnections())
+                    {
+                        auto interconnectionItem = static_cast<HWConnection*>(connection);
+                        design->removeRoute(interconnectionItem->getRouteExtension());
+                        design->getInterconnections()->removeOne(interconnectionItem->getInterconnection());
+                    }
+                }
+            }
+
+            designComponentInterfaceIt = designComponentInterfaces.erase(designComponentInterfaceIt);
+        }
+        else
+        {
+            ++designComponentInterfaceIt;
+        }
+    }
+
+    QSharedPointer<QList<QSharedPointer<BusInterface> > >newBuses(new QList(designComponentInterfaces));
+    getEditedComponent()->setBusInterfaces(newBuses);
+}
+
+//-----------------------------------------------------------------------------
+// Function: HWDesignWidget::loadPortChanges()
+//-----------------------------------------------------------------------------
+void HWDesignWidget::loadPortChanges(QSharedPointer<Component const> libraryComponent)
+{
+    auto design = getDiagram()->getDesign();
+    auto designComponentPorts = QList<QSharedPointer<Port> >(*getEditedComponent()->getPorts());
+    auto libraryComponentPorts = QList<QSharedPointer<Port> >(*libraryComponent->getPorts());
+
+    // First check for added ports
+    for (auto const& compPort : libraryComponentPorts)
+    {
+        // Replace if found with same name
+        if (auto foundPort = Search::findByName(compPort->name(), designComponentPorts))
+        {
+            designComponentPorts.removeOne(foundPort);
+        }
+
+        designComponentPorts.append(compPort);
+    }
+
+    // Remove deleted ports (aka if port exists in deisgn component but not in component loaded from disk)
+    for (auto designComponentPortIt = designComponentPorts.begin(); designComponentPortIt != designComponentPorts.end();)
+    {
+        if (auto foundPort = Search::findByName((*designComponentPortIt)->name(), libraryComponentPorts);
+            foundPort == nullptr)
+        {
+            // Remove from design
+            auto adHocVisibilities = getDiagram()->getDesign()->getAdHocPortPositions().dynamicCast<Kactus2Group>();
+            for (auto const& adHocVisibility : adHocVisibilities->getByType("kactus2:adHocVisible"))
+            {
+                if (adHocVisibility.dynamicCast<Kactus2Placeholder>()->getAttributeValue("portName") == (*designComponentPortIt)->name())
+                {
+                    adHocVisibilities->removeFromGroup(adHocVisibility);
+                    break;
+                }
+            }
+
+            // Remove connections and routes
+            if (auto portItem = getDiagram()->getDiagramAdHocPort((*designComponentPortIt)->name()))
+            {
+                for (auto const& connection : portItem->getConnections())
+                {
+                    auto adHocConnection = static_cast<AdHocConnectionItem*>(connection);
+                    getDiagram()->getDesign()->removeRoute(adHocConnection->getRouteExtension());
+                    getDiagram()->getDesign()->getAdHocConnections()->removeOne(adHocConnection->getInterconnection());
+                }
+            }
+
+            designComponentPortIt = designComponentPorts.erase(designComponentPortIt);
+        }
+        else
+        {
+            ++designComponentPortIt;
+        }
+    }
+
+    QSharedPointer<QList<QSharedPointer<Port> > >newPortsQ(new QList(designComponentPorts));
+    getEditedComponent()->setPorts(newPortsQ);
+}
+
+//-----------------------------------------------------------------------------
 // Function: HWDesignWidget::addColumn()
 //-----------------------------------------------------------------------------
 void HWDesignWidget::addColumn()

editors/common/DesignWidget.cpp
@@ -227,8 +227,6 @@ QSharedPointer<IEditProvider> DesignWidget::getEditProvider() const
 //-----------------------------------------------------------------------------
 bool DesignWidget::save()
 {
-    getDiagram()->updateHierComponent();
-
     // Create the design.
     QSharedPointer<Design> design = getDiagram()->getDesign();
     QSharedPointer<DesignConfiguration> designConf = getDiagram()->getDesignConfiguration();

version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           3.13.346.0
+#define VERSION_FULL           3.13.427.0
 
 #define VERSION_BASEYEAR       0
-#define VERSION_DATE           "2024-06-19"
-#define VERSION_TIME           "11:42:48"
+#define VERSION_DATE           "2024-06-28"
+#define VERSION_TIME           "13:52:57"
 
 #define VERSION_MAJOR          3
 #define VERSION_MINOR          13
-#define VERSION_BUILDNO        346
+#define VERSION_BUILDNO        427
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           3,13,346,0
-#define VERSION_PRODUCT        3,13,346,0
-#define VERSION_FILESTR        "3,13,346,0"
-#define VERSION_PRODUCTSTR     "3,13,346,0"
+#define VERSION_FILE           3,13,427,0
+#define VERSION_PRODUCT        3,13,427,0
+#define VERSION_FILESTR        "3,13,427,0"
+#define VERSION_PRODUCTSTR     "3,13,427,0"
 
 #endif

common/widgets/tabDocument/TabDocument.cpp
@@ -269,6 +269,14 @@ bool TabDocument::askSaveFile() const {
 }
 
 //-----------------------------------------------------------------------------
+// Function: TabDocument::loadChangesFromSibling()
+//-----------------------------------------------------------------------------
+void TabDocument::loadChangesFromRelatedTab()
+{
+    // Implementation left for derived classes, if needed
+}
+
+//-----------------------------------------------------------------------------
 // Function: TabDocument::updateTabTitle()
 //-----------------------------------------------------------------------------
 void TabDocument::updateTabTitle()
@@ -454,6 +462,7 @@ void TabDocument::handleRefreshRequest()
 
         if (msgBox.exec() == QMessageBox::Yes)
         {
+            loadChangesFromRelatedTab();
             save();
             refresh();
         }

common/widgets/tabDocument/TabDocument.h
@@ -375,6 +375,12 @@ private:
     TabDocument& operator=(TabDocument const& rhs);
 
     /*!
+     *	Load changes made in related tabs. Reimplement to load editor/tab-specific changes. 
+     *  Reimplementations should only load changes that can't cause conflicts.
+     */
+    virtual void loadChangesFromRelatedTab();
+
+    /*!
      *  Updates the tab title.
      */
     void updateTabTitle();

editors/HWDesign/AdHocItem.cpp
@@ -222,6 +222,14 @@ QSharedPointer<Port> AdHocItem::getPort() const
 }
 
 //-----------------------------------------------------------------------------
+// Function: AdHocItem::setPort()
+//-----------------------------------------------------------------------------
+void AdHocItem::setPort(QSharedPointer<Port> newPort)
+{
+    port_ = newPort;
+}
+
+//-----------------------------------------------------------------------------
 // Function: AdHocItem::changeTieOffLabel()
 //-----------------------------------------------------------------------------
 void AdHocItem::changeTieOffLabel(QString const& tieOffExpression, QString const& tieOffValue,

commit d60a1fbc774a6b840dad5eb8b3f614938d5a6f93

[CORRECTIVE] Fix design component bus interfaces loading from component. Fixes bugs introduced in fc14c8aa3d89f39061511510eb1ca9876e82dd8b.

editors/HWDesign/HWDesignDiagram.cpp
@@ -243,14 +243,14 @@ void HWDesignDiagram::updateHierComponent()
     QList<QSharedPointer<BusInterface> > busIfs;
 
     // Search all graphics items in the scene.
-    for (QGraphicsItem *item : items())
+    for (QGraphicsItem* item : items())
     {
         // Check if the item is a diagram interface and its bus interface is defined.
         if (auto diagIf = dynamic_cast<HierarchicalBusInterfaceItem*>(item))
         {
             if (diagIf->getBusInterface() != nullptr && !diagIf->isInvalid())
             {
-			    busIfs.append(diagIf->getBusInterface());
+                busIfs.append(diagIf->getBusInterface());
             }
         }
     }
@@ -271,26 +271,30 @@ void HWDesignDiagram::updateHierComponent()
         }
     }
 
-    QList<QSharedPointer<BusInterface> > toRemove;
-
-    // Remove interfaces deleted in the design from the component.
-    for (auto componentInterface : *componentInterfaces)
+    if (manuallyDeletedInterfaces_)
     {
-        bool busExists = std::find_if(busIfs.cbegin(), busIfs.cend(), [&componentInterface](auto diagramInterface)
-            {
-                return componentInterface->name() == diagramInterface->name();
-            }) != busIfs.cend();
-
-        if (!busExists)
+        QList<QSharedPointer<BusInterface> > toRemove;
+        // Remove interfaces deleted in the design from the component.
+        for (auto componentInterface : *componentInterfaces)
         {
-            toRemove.append(componentInterface);
+            bool busExists = std::find_if(busIfs.cbegin(), busIfs.cend(), [&componentInterface](auto diagramInterface)
+                {
+                    return componentInterface->name() == diagramInterface->name();
+                }) != busIfs.cend();
+
+            if (!busExists)
+            {
+                toRemove.append(componentInterface);
+            }
+        }
+
+        for (auto interfaceToRemove : toRemove)
+        {
+            componentInterfaces->removeAll(interfaceToRemove);
         }
     }
 
-    for (auto interfaceToRemove : toRemove)
-    {
-        componentInterfaces->removeAll(interfaceToRemove);
-    }
+    manuallyDeletedInterfaces_ = false;
 }
 
 //-----------------------------------------------------------------------------
@@ -408,6 +412,14 @@ HWConnectionEndpoint* HWDesignDiagram::getHierarchicalInterface(QString const& b
     }
 
     return nullptr;
+}
+
+//-----------------------------------------------------------------------------
+// Function: HWDesignDiagram::getInterfacesToDelete()
+//-----------------------------------------------------------------------------
+void HWDesignDiagram::setInterfacesHaveBeenDeleted(bool haveBeenDeleted)
+{
+    manuallyDeletedInterfaces_ = true;
 }
 
 //-----------------------------------------------------------------------------

editors/HWDesign/HWDesignDiagram.h
@@ -117,7 +117,7 @@ public:
     {
         QSharedPointer<Component> component;            //!< The referenced component.
         QSharedPointer<ComponentInstance> instance;
-       
+
         ComponentInstanceCopyData()
             : component(),
             instance()
@@ -155,8 +155,8 @@ public:
          */
         ColumnCopyData()
             : desc(),
-              components(),
-              interfaces()
+            components(),
+            interfaces()
         {
         }
     };
@@ -176,7 +176,7 @@ public:
         {
         }
     };
-    
+
     /*!
      *  The constructor.
      *
@@ -186,15 +186,15 @@ public:
      *      @param [in] designParameterFinder               Parameter finder for design parameters.
      *      @param [in] parent                              Owner of this diagram.
      */
-    HWDesignDiagram(LibraryInterface *lh, QSharedPointer<IEditProvider> editProvider,
+    HWDesignDiagram(LibraryInterface* lh, QSharedPointer<IEditProvider> editProvider,
         QSharedPointer<MultipleParameterFinder> designAndInstancesParameterFinder,
         QSharedPointer<ListParameterFinder> designParameterFinder,
         DesignWidget* parent = 0);
 
-	/*! 
+    /*!
      *  The destructor.
      */
-	~HWDesignDiagram() final = default;
+    ~HWDesignDiagram() final = default;
 
     /*!
      *  Set the IP-XACT document that is viewed in HWDesignDiagram.
@@ -206,7 +206,7 @@ public:
      */
     HWComponentItem* getComponentItem(QString const& instanceName);
 
-	/*!
+    /*!
      *  Reflects the changes in the design to the top-level component.
      */
     virtual void updateHierComponent();
@@ -239,14 +239,21 @@ public:
     virtual HWConnectionEndpoint* getDiagramAdHocPort(QString const& portName);
 
     /*!
-     *	Returns the bus interface item of the corresponding design component bus interface with the given name 
+     *	Returns the bus interface item of the corresponding design component bus interface with the given name
      *  or nullptr if not found.
-     *  
+     *
      *      @param [in] busRef     The interface name.
-     *	    
+     *
      * 	    @return The hierarchical bus interface item found or null.
      */
     HWConnectionEndpoint* getHierarchicalInterface(QString const& busRef) const;
+
+    /*!
+     *	Sets the value of the flag indicating if interfaces have been deleted in the design diagram.
+     *  
+     *      @param [in] haveBeenDeleted     Flag value to set.
+     */
+    void setInterfacesHaveBeenDeleted(bool haveBeenDeleted);
 
 public slots:
 
@@ -926,6 +933,9 @@ private:
 
     //! Parameter finder for design parameter reference tree
     QSharedPointer<MultipleParameterFinder> designAndInstancesParameterFinder_;
+
+    //! Flag indicating if interfaces have been deleted from the design diagram.
+    bool manuallyDeletedInterfaces_ = false;
 };
 
 #endif // HWDESIGNDIAGRAM_H

editors/HWDesign/HWDesignWidget.cpp
@@ -514,6 +514,7 @@ void HWDesignWidget::deleteSelectedBusInterfaceItems(QList<QGraphicsItem*> selec
             new InterfaceDeleteCommand(getDiagram(), diagIf, removePorts, cmd.data());
         connect(childCmd, SIGNAL(interfaceDeleted()), this, SIGNAL(clearItemSelection()), Qt::UniqueConnection);            
 
+        static_cast<HWDesignDiagram*>(getDiagram())->setInterfacesHaveBeenDeleted(true);
         childCmd->redo();
     }
 
@@ -917,14 +918,15 @@ void HWDesignWidget::loadBusInterfaceChanges(QSharedPointer<Component const> lib
         designComponentInterfaces.append(compBusif);
     }
 
-    // Remove deleted interfaces (aka if interface exists in design component but not in component loaded from disk)
+    auto hwDiagram = static_cast<HWDesignDiagram*>(getDiagram());
+   
+    // Remove deleted interfaces (aka if interface exists in design component but not in component loaded from disk).
     for (auto designComponentInterfaceIt = designComponentInterfaces.begin(); designComponentInterfaceIt != designComponentInterfaces.end();)
     {
         if (auto foundInterface = Search::findByName((*designComponentInterfaceIt)->name(), libraryComponentInterfaces);
             foundInterface == nullptr)
         {
             // Remove connections and routes from design, if interface had no connections
-            auto hwDiagram = static_cast<HWDesignDiagram*>(getDiagram());
             if (auto endpointItem = hwDiagram->getHierarchicalInterface((*designComponentInterfaceIt)->name()))
             {
                 auto busItem = static_cast<HierarchicalBusInterfaceItem*>(endpointItem);
@@ -939,6 +941,8 @@ void HWDesignWidget::loadBusInterfaceChanges(QSharedPointer<Component const> lib
                         design->getInterconnections()->removeOne(interconnectionItem->getInterconnection());
                     }
                 }
+
+                hwDiagram->removeItem(busItem);
             }
 
             designComponentInterfaceIt = designComponentInterfaces.erase(designComponentInterfaceIt);

editors/common/DesignWidget.cpp
@@ -227,6 +227,8 @@ QSharedPointer<IEditProvider> DesignWidget::getEditProvider() const
 //-----------------------------------------------------------------------------
 bool DesignWidget::save()
 {
+    getDiagram()->updateHierComponent();
+
     // Create the design.
     QSharedPointer<Design> design = getDiagram()->getDesign();
     QSharedPointer<DesignConfiguration> designConf = getDiagram()->getDesignConfiguration();

version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           3.13.484.0
+#define VERSION_FULL           3.13.513.0
 
 #define VERSION_BASEYEAR       0
-#define VERSION_DATE           "2024-07-08"
-#define VERSION_TIME           "13:18:48"
+#define VERSION_DATE           "2024-07-10"
+#define VERSION_TIME           "13:40:57"
 
 #define VERSION_MAJOR          3
 #define VERSION_MINOR          13
-#define VERSION_BUILDNO        484
+#define VERSION_BUILDNO        513
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           3,13,484,0
-#define VERSION_PRODUCT        3,13,484,0
-#define VERSION_FILESTR        "3,13,484,0"
-#define VERSION_PRODUCTSTR     "3,13,484,0"
+#define VERSION_FILE           3,13,513,0
+#define VERSION_PRODUCT        3,13,513,0
+#define VERSION_FILESTR        "3,13,513,0"
+#define VERSION_PRODUCTSTR     "3,13,513,0"
 
 #endif