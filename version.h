da2498a2ff3b8be6f32fc95bd9338d248cd5a7fa
hagantsa <anton.hagqvist@tuni.fi>
2025-04-14 13:45:59 +0300

    [CORRECTIVE] Fix removal of non-contiguous rows in tables using EditableTableView. Fixes #122

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           3.13.366.0
+#define VERSION_FULL           3.13.379.0
 
 #define VERSION_BASEYEAR       0
-#define VERSION_DATE           "2025-04-09"
-#define VERSION_TIME           "16:15:18"
+#define VERSION_DATE           "2025-04-14"
+#define VERSION_TIME           "13:35:41"
 
 #define VERSION_MAJOR          3
 #define VERSION_MINOR          13
-#define VERSION_BUILDNO        366
+#define VERSION_BUILDNO        379
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           3,13,366,0
-#define VERSION_PRODUCT        3,13,366,0
-#define VERSION_FILESTR        "3,13,366,0"
-#define VERSION_PRODUCTSTR     "3,13,366,0"
+#define VERSION_FILE           3,13,379,0
+#define VERSION_PRODUCT        3,13,379,0
+#define VERSION_FILESTR        "3,13,379,0"
+#define VERSION_PRODUCTSTR     "3,13,379,0"
 
 #endif
