1807cb29b80c860c10bee15735bf34666131ab2c
hagantsa <anton.hagqvist@tuni.fi>
2025-05-05 13:15:45 +0300

    [CORRECTIVE] Fix deleting rows in editable table views with no sort filter

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           3.13.6.0
+#define VERSION_FULL           3.13.12.0
 
 #define VERSION_BASEYEAR       0
-#define VERSION_DATE           "2025-04-29"
-#define VERSION_TIME           "12:55:03"
+#define VERSION_DATE           "2025-05-05"
+#define VERSION_TIME           "13:09:43"
 
 #define VERSION_MAJOR          3
 #define VERSION_MINOR          13
-#define VERSION_BUILDNO        6
+#define VERSION_BUILDNO        12
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           3,13,6,0
-#define VERSION_PRODUCT        3,13,6,0
-#define VERSION_FILESTR        "3,13,6,0"
-#define VERSION_PRODUCTSTR     "3,13,6,0"
+#define VERSION_FILE           3,13,12,0
+#define VERSION_PRODUCT        3,13,12,0
+#define VERSION_FILESTR        "3,13,12,0"
+#define VERSION_PRODUCTSTR     "3,13,12,0"
 
 #endif
