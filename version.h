9631a0e3d95181ee70488662ce779dd1bd2af0de
hagantsa <anton.hagqvist@tuni.fi>
2025-05-16 16:06:54 +0300

    [CORRECTIVE] Fix changes to ports/bus interfaces reflecting wrongly in design/component editor. Adhoc connections to top level port are no longer removed when port is deleted from component.

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           3.13.51.0
+#define VERSION_FULL           3.13.71.0
 
 #define VERSION_BASEYEAR       0
-#define VERSION_DATE           "2025-05-15"
-#define VERSION_TIME           "13:57:47"
+#define VERSION_DATE           "2025-05-16"
+#define VERSION_TIME           "15:18:57"
 
 #define VERSION_MAJOR          3
 #define VERSION_MINOR          13
-#define VERSION_BUILDNO        51
+#define VERSION_BUILDNO        71
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           3,13,51,0
-#define VERSION_PRODUCT        3,13,51,0
-#define VERSION_FILESTR        "3,13,51,0"
-#define VERSION_PRODUCTSTR     "3,13,51,0"
+#define VERSION_FILE           3,13,71,0
+#define VERSION_PRODUCT        3,13,71,0
+#define VERSION_FILESTR        "3,13,71,0"
+#define VERSION_PRODUCTSTR     "3,13,71,0"
 
 #endif
