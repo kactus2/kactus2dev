c792ee29ab585cf368e4e28c699be93a438fbc5f
hanhisua <teemu.hanhisuanto@nokia.com>
2025-05-30 13:34:46 +0300

    [CORRECTIVE] Restore call to 'writeAxiXbarCfg' for AXI4LITE

--- KactusAPI/plugins/InterconnectGenerator/InterconnectRTLWriter.cpp
+++ KactusAPI/plugins/InterconnectGenerator/InterconnectRTLWriter.cpp
@@ -77,6 +77,7 @@ void InterconnectRTLWriter::generateRTL()
 
             writeAXI4LITE(verilogRTL);
             writeAddrMap(verilogRTL);
+            writeAxiXbarCfg(verilogRTL);
             writeAxiXbar(verilogRTL);
 
         } else {
