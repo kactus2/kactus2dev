kactus2/kactus2dev

commit 90f6016057f25a261d5444e7d5bae2ec55e662af

[CORRECTIVE] Improve tab and shift+tab behaviour in VLNV editor. Fixes crash in #121

common/widgets/assistedLineEdit/AssistedLineEdit.cpp
@@ -24,9 +24,20 @@
 //-----------------------------------------------------------------------------
 AssistedLineEdit::AssistedLineEdit(QWidget* parentWnd, QWidget* parent) : 
 QLineEdit(parent),
-contentAssist_(new LineContentAssistWidget(this, parentWnd))
+contentAssist_(new LineContentAssistWidget(this, parentWnd)) // NOTE: parent window set as parent so that 
+                                                             // content assist visibility isn't dependent 
+                                                             // on "real" parent widget size!
 {
     contentAssist_->setFocusProxy(this);
+}
+
+//-----------------------------------------------------------------------------
+// Function: AssistedLineEdit::~AssistedLineEdit()
+//-----------------------------------------------------------------------------
+AssistedLineEdit::~AssistedLineEdit()
+{
+    // Change parent of content assist from parent window to destroy object with the line edit
+    contentAssist_->setParent(this);
 }
 
 //-----------------------------------------------------------------------------

common/widgets/assistedLineEdit/AssistedLineEdit.h
@@ -41,7 +41,7 @@ public:
     /*!
      *  The destructor.
      */
-    virtual ~AssistedLineEdit() = default;
+    virtual ~AssistedLineEdit();
 
     // Disable copying.
     AssistedLineEdit(AssistedLineEdit const& rhs) = delete;

version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           3.13.12.0
+#define VERSION_FULL           3.13.35.0
 
 #define VERSION_BASEYEAR       0
-#define VERSION_DATE           "2025-05-05"
-#define VERSION_TIME           "13:09:43"
+#define VERSION_DATE           "2025-05-08"
+#define VERSION_TIME           "14:00:51"
 
 #define VERSION_MAJOR          3
 #define VERSION_MINOR          13
-#define VERSION_BUILDNO        12
+#define VERSION_BUILDNO        35
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           3,13,12,0
-#define VERSION_PRODUCT        3,13,12,0
-#define VERSION_FILESTR        "3,13,12,0"
-#define VERSION_PRODUCTSTR     "3,13,12,0"
+#define VERSION_FILE           3,13,35,0
+#define VERSION_PRODUCT        3,13,35,0
+#define VERSION_FILESTR        "3,13,35,0"
+#define VERSION_PRODUCTSTR     "3,13,35,0"
 
 #endif

common/widgets/vlnvEditor/vlnveditor.cpp
@@ -508,6 +508,53 @@ void VLNVEditor::showEvent(QShowEvent* event)
 }
 
 //-----------------------------------------------------------------------------
+// Function: VLNVEditor::focusNextPrevChild()
+//-----------------------------------------------------------------------------
+bool VLNVEditor::focusNextPrevChild(bool next)
+{
+    // Function overridden to fix tab and shift+tab behavior when created by delegate of EditableTableView.
+
+    if (next)
+    {
+        if (vendorEdit_->hasFocus())
+        {
+            libraryEdit_->setFocus();
+            return true;
+        }
+        else if (libraryEdit_->hasFocus())
+        {
+            nameEdit_->setFocus();
+            return true;
+        }
+        else if (nameEdit_->hasFocus())
+        {
+            versionEdit_->setFocus();
+            return true;
+        }
+    }
+    else
+    {
+        if (libraryEdit_->hasFocus())
+        {
+            vendorEdit_->setFocus();
+            return true;
+        }
+        else if (nameEdit_->hasFocus())
+        {
+            libraryEdit_->setFocus();
+            return true;
+        }
+        else if (versionEdit_->hasFocus())
+        {
+            nameEdit_->setFocus();
+            return true;
+        }
+    }
+
+    return QWidget::focusNextPrevChild(next);
+}
+
+//-----------------------------------------------------------------------------
 // Function: VLNVEditor::refresh()
 //-----------------------------------------------------------------------------
 void VLNVEditor::refresh()

common/widgets/vlnvEditor/vlnveditor.h
@@ -215,6 +215,15 @@ protected:
      *    @param [in] event   Pointer to the show event.
      */
     virtual void showEvent(QShowEvent* event);
+    
+    /*!
+     *	Finds a new widget to give the keyboard focus to with tab and shift+tab.
+     *  
+     *    @param [in] next     Searches forward if true, otherwise searches backwards.
+     *	    
+     *    @return True if it can find a new widget, false if it can't
+     */
+    bool focusNextPrevChild(bool next) override;
 
 private slots:
 

commit 3aaa7a2774f8a36d409ba69e24bf408127996635

[CORRECTIVE] Fix vlnv editor crashing in some situations. Fixes editor crashing when child widgets are deleted in unintended order, bug introduced in 90f6016057f25a261d5444e7d5bae2ec55e662af.

common/widgets/assistedLineEdit/AssistedLineEdit.cpp
@@ -32,15 +32,6 @@ contentAssist_(new LineContentAssistWidget(this, parentWnd)) // NOTE: parent win
 }
 
 //-----------------------------------------------------------------------------
-// Function: AssistedLineEdit::~AssistedLineEdit()
-//-----------------------------------------------------------------------------
-AssistedLineEdit::~AssistedLineEdit()
-{
-    // Change parent of content assist from parent window to destroy object with the line edit
-    contentAssist_->setParent(this);
-}
-
-//-----------------------------------------------------------------------------
 // Function: keyPressEvent()
 //-----------------------------------------------------------------------------
 void AssistedLineEdit::keyPressEvent(QKeyEvent* e)

common/widgets/assistedLineEdit/AssistedLineEdit.h
@@ -41,7 +41,7 @@ public:
     /*!
      *  The destructor.
      */
-    virtual ~AssistedLineEdit();
+    virtual ~AssistedLineEdit() = default;
 
     // Disable copying.
     AssistedLineEdit(AssistedLineEdit const& rhs) = delete;

version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           3.13.72.0
+#define VERSION_FULL           3.13.73.0
 
 #define VERSION_BASEYEAR       0
-#define VERSION_DATE           "2025-05-19"
-#define VERSION_TIME           "10:23:23"
+#define VERSION_DATE           "2025-06-03"
+#define VERSION_TIME           "13:09:55"
 
 #define VERSION_MAJOR          3
 #define VERSION_MINOR          13
-#define VERSION_BUILDNO        72
+#define VERSION_BUILDNO        73
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           3,13,72,0
-#define VERSION_PRODUCT        3,13,72,0
-#define VERSION_FILESTR        "3,13,72,0"
-#define VERSION_PRODUCTSTR     "3,13,72,0"
+#define VERSION_FILE           3,13,73,0
+#define VERSION_PRODUCT        3,13,73,0
+#define VERSION_FILESTR        "3,13,73,0"
+#define VERSION_PRODUCTSTR     "3,13,73,0"
 
 #endif