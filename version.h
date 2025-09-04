5dd94512497e6a6711371739e52a1de72b3289c6
teuhom <mikko.teuho@tuni.fi>
2025-09-04 12:04:18 +0300

    [CORRECTIVE] Updated port map auto connector #129

--- version.h
+++ version.h
@@ -10,20 +10,20 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           3.13.153.0
+#define VERSION_FULL           3.13.168.0
 
 #define VERSION_BASEYEAR       0
-#define VERSION_DATE           "2025-08-21"
-#define VERSION_TIME           "10:03:25"
+#define VERSION_DATE           "2025-09-04"
+#define VERSION_TIME           "12:01:32"
 
 #define VERSION_MAJOR          3
 #define VERSION_MINOR          13
-#define VERSION_BUILDNO        153
+#define VERSION_BUILDNO        168
 #define VERSION_EXTEND         0
 
-#define VERSION_FILE           3,13,153,0
-#define VERSION_PRODUCT        3,13,153,0
-#define VERSION_FILESTR        "3,13,153,0"
-#define VERSION_PRODUCTSTR     "3,13,153,0"
+#define VERSION_FILE           3,13,168,0
+#define VERSION_PRODUCT        3,13,168,0
+#define VERSION_FILESTR        "3,13,168,0"
+#define VERSION_PRODUCTSTR     "3,13,168,0"
 
 #endif
